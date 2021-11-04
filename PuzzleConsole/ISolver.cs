e3ec7a7c56c1c7ac21e2ae531ec8565e05c9547c
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-11-04 09:36:17 +0100

    first versions

---
+++ PuzzleConsole/ISolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleConsole
+{
+    public interface ISolver
+    {
+        public string[] Solve(string[] puzzle);
+    }
+}
---
+++ PuzzleConsole/Year2015/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleConsole.Year2015
+{
+    public class Day1 : ISolver
+    {
+        public string[] Solve(string[] puzzle)
+        {
+            return puzzle[0..1];
+        }
+    }
+}
