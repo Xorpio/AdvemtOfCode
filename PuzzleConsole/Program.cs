873b853fb1a3a281c3db92874a72aa9fa85b3b57
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-11-04 09:19:10 +0100

    inital commit

---
+++ PuzzleConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PuzzleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
