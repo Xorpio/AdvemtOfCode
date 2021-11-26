920447383b9784bdb2dffa2cd9f975850e0d001a
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-11-26 13:21:08 +0100

    added day1 tests

---
+++ PuzzleConsole.test/Year2015/Day1Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleConsole.test.Year2015
+{
+    internal class Day1Test
+    {
+    }
+}
