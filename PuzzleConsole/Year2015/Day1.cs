dc5dc6e2a49b1e6db49573de2017f77c02b9ebbb
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-12-10 10:33:57 +0100

    2021 day1

--- PuzzleConsole/Year2015/Day1.cs
+++ PuzzleConsole/Year2015/Day1.cs
@@ -1,9 +1,70 @@
-﻿namespace PuzzleConsole.Year2015;
+﻿using System.Text.RegularExpressions;
+
+namespace PuzzleConsole.Year2015;
 
 public class Day1 : ISolver
 {
+    private const decimal Start = 20151125;
+    private const decimal Multiply = 252533;
+    private const decimal Modulo = 33554393;
+
+    private int _row = 0;
+    private int _col = 0;
+
+    private IList<IList<decimal>> _grid;
     public string[] Solve(string[] puzzle)
     {
-        return puzzle[0..1];
+        string pattern = @"\d+,\d+";
+        Regex puzzleInputRegex = new Regex(pattern);
+        if (puzzle == null || puzzle.Length < 1 || string.IsNullOrWhiteSpace(puzzle[0]) || !puzzleInputRegex.IsMatch(puzzle[0]))
+        {
+            throw new InvalidParameterException("Puzzle input not valid. Expected '(decimal),(decimal)'");
+        }
+
+        _grid = new List<IList<decimal>>();
+        _grid.Add(new List<decimal>()
+        {
+           Start
+        });
+
+        var row = int.Parse(puzzle[0].Split(',')[0]) - 1;
+        var column = int.Parse(puzzle[0].Split(',')[1]) - 1;
+
+        while (_grid.ElementAtOrDefault(row)?.ElementAtOrDefault(column) is null)
+        {
+            addToGrid();
+        }
+
+        return new string[] { _grid[_row][_col].ToString() };
+    }
+
+    private void addToGrid()
+    {
+        var cur = _grid[_row][_col];
+        var next = (cur * Multiply) % Modulo;
+
+        var newRow = _row - 1;
+        var newCol = _col + 1;
+
+        if (newRow < 0)
+        {
+            newRow = _grid.Count;
+            newCol = 0;
+        }
+
+        _row = newRow;
+        _col = newCol;
+
+        if (_grid.ElementAtOrDefault(_row) is null)
+        {
+            _grid.Add(new List<decimal>());
+        }
+        _grid[_row].Add(next);
+    }
+}
+public class InvalidParameterException : Exception
+{
+    public InvalidParameterException(string message) : base(message)
+    {
     }
 }
---
+++ PuzzleConsole/Year2021/Day1.cs
@@ -0,0 +1,36 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace PuzzleConsole.Year2021;
+
+public class Day1 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+        var source = new Subject<int>();
+
+        int val = 0;
+        int val2 = 0;
+        source.Buffer<int>(2, 1)
+            .Select(b => b.Count() == 2 && b[0] < b[1] ? 1 : 0)
+            .Sum()
+            .Subscribe(v => val = v);
+
+        source.Buffer<int>(3, 1)
+            .Select(b => b.Sum())
+            .Buffer<int>(2, 1)
+            .Select(b => b.Count() == 2 && b[0] < b[1] ? 1 : 0)
+            .Sum()
+            .Subscribe(v => val2 = v);
+
+        foreach(var item in puzzle)
+        {
+            int.TryParse(item, out var depth);
+            source.OnNext(depth);
+        }
+
+        source.OnCompleted();
+
+        return new string[] { val.ToString(), val2.ToString() };
+    }
+}
