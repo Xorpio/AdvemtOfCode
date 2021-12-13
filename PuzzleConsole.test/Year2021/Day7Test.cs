20cb46ea1e5d948429e57b43109a7b85d5b257b2
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-12-13 13:59:33 +0100

    added day 7

---
+++ PuzzleConsole.test/Year2021/Day7Test.cs
@@ -0,0 +1,20 @@
+﻿using PuzzleConsole.Year2021;
+using Xunit;
+
+namespace PuzzleConsole.test.Year2021;
+
+public class Day7Test
+{
+    [Fact]
+    public void SolveTest()
+    {
+        var sut = new Day7();
+
+        var puzzle = new string[] { "16,1,2,0,4,2,7,1,2,14" };
+
+        var solution = sut.Solve(puzzle);
+
+        solution.First().Should().Be("37");
+        solution.Last().Should().Be("168");
+    }
+}
---
+++ PuzzleConsole/Year2021/Day7.cs
@@ -0,0 +1,32 @@
+﻿using System.Reactive.Linq;
+
+namespace PuzzleConsole.Year2021;
+
+public class Day7 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+        var fishList = puzzle[0].Split(',')
+            .Select(s => int.Parse(s))
+            .ToList();
+
+        var solutions = new List<int>();
+        var solutions2 = new List<int>();
+        for (int i = fishList.Min(); i <= fishList.Max(); i++)
+        {
+            var fuel = 0;
+            var fuel2 = 0;
+            foreach (var fish in fishList)
+            {
+                var f = Math.Abs(fish - i);
+                fuel += f;
+                fuel2 += Enumerable.Range(1, f).Sum();
+            }
+            solutions.Add(fuel);
+            solutions2.Add(fuel2);
+        }
+
+        return new string[] { solutions.Min().ToString() ,
+         solutions2.Min().ToString() };
+    }
+}
