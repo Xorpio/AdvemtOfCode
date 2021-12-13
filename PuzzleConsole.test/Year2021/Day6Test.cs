2f572bc070a3f915a31bf340d58d9080bf1d797b
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-12-13 13:22:38 +0100

    added day 6

---
+++ PuzzleConsole.test/Year2021/Day6Test.cs
@@ -0,0 +1,22 @@
+﻿using PuzzleConsole.Year2021;
+using Xunit;
+
+namespace PuzzleConsole.test.Year2021;
+
+public class Day6Test
+{
+    [Fact]
+    public void SolveTest()
+    {
+        var sut = new Day6();
+
+        var puzzle = new string[] { "3,4,3,1,2" };
+
+        var solution = sut.Solve(puzzle);
+
+        solution.Should().HaveCount(3);
+        solution.First().Should().Be("Day 18: 26");
+        solution[1].Should().Be("Day 80: 5934");
+        solution[2].Should().Be("Day 256: 26984457539");
+    }
+}
---
+++ PuzzleConsole/Year2021/Day6.cs
@@ -0,0 +1,77 @@
+﻿using System.Reactive.Linq;
+
+namespace PuzzleConsole.Year2021;
+
+public class Day6 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+        var fishList = puzzle[0].Split(',')
+            .Select(s => int.Parse(s))
+            .ToList();
+
+        var fishes = new Dictionary<int, decimal>();
+        for (int i = 0; i < 9; i++)
+        {
+            fishes.Add(i, 0);
+        }
+
+        foreach (var sourceFish in fishList)
+        {
+            fishes[sourceFish]++;
+        }
+
+        decimal total18 = 0;
+        decimal total80 = 0;
+        decimal total256 = 0;
+        for (int i = 0; i < 256; i++)
+        {
+            var newFishes = new Dictionary<int, decimal>();
+            for (int c = 0; c < 9; c++)
+            {
+                newFishes.Add(c, 0);
+            }
+
+            foreach (var fish in fishes)
+            {
+                if (fish.Key == 0)
+                {
+                    newFishes[6] += fish.Value;
+                    newFishes[8] += fish.Value;
+                }
+                else
+                {
+                    newFishes[fish.Key -1] += fish.Value;
+                }
+            }
+
+            fishes = newFishes;
+            if (i == 17)
+            { 
+                foreach (var fish in fishes)
+                {
+                    total18 += fish.Value;
+                }
+            }
+            if (i == 79)
+            { 
+                foreach (var fish in fishes)
+                {
+                    total80 += fish.Value;
+                }
+            }
+        }
+
+        foreach (var fish in fishes)
+        {
+            total256 += fish.Value;
+        }
+
+
+        return new string[] {
+            $"Day 18: {total18}",
+            $"Day 80: {total80}",
+            $"Day 256: {total256}"
+        };
+    }
+}
