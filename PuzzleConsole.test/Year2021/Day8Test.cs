c1bdcf38dc24d5e9512e319b17e0842652fefcfc
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-12-14 11:29:09 +0100

    added day 8

---
+++ PuzzleConsole.test/Year2021/Day8Test.cs
@@ -0,0 +1,43 @@
+﻿using PuzzleConsole.Year2021;
+using Xunit;
+
+namespace PuzzleConsole.test.Year2021;
+
+public class Day8Test
+{
+    [Fact]
+    public void SolveTest()
+    {
+        var sut = new Day8();
+
+        var puzzle = new string[] { 
+                "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+                "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+                "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+                "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+                "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+                "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+                "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+                "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+                "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+                "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+            };
+
+        var solution = sut.Solve(puzzle);
+
+        solution.First().Should().Be("26");
+        solution.Last().Should().Be("61229");
+    }
+
+    [Fact]
+    public void SolveTest2()
+    {
+        var sut = new Day8();
+
+        var puzzle = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+
+        var solution = sut.SolveLine(puzzle);
+
+        solution.Should().Be(5353);
+    }
+}
---
+++ PuzzleConsole/Year2021/Day8.cs
@@ -0,0 +1,74 @@
+﻿using System.Reactive.Linq;
+
+namespace PuzzleConsole.Year2021;
+
+public class Day8 : ISolver
+{
+    public string[] Solve(string[] puzzleInput)
+    {
+        var puzzle = puzzleInput.Select(l => l.Split(" | "))
+            .Select(ls => new { Segments = ls[0], Numbers = ls[1].Split(' ') })
+            .ToList();
+
+        var lengts = puzzle.SelectMany(p => p.Numbers)
+            .Select(s => s.Length);
+
+        var answer = lengts.Where(l => l == 2 || l == 3 || l == 4 || l == 7).Count();
+
+        var answer2 = puzzleInput.Select(l => SolveLine(l)).Sum();
+
+        return new string[] { answer.ToString(), answer2.ToString() };
+    }
+
+    public int SolveLine (string input)
+    {
+        var linesplit = input.Split(" | ");
+        var segments = linesplit[0].Split(' ').Select(s => string.Concat(s.OrderBy(c => c)));
+        var numbers = linesplit[1].Split(' ').Select(s => string.Concat(s.OrderBy(c => c)));
+
+        var digits = new Dictionary<string, string>();
+
+        digits.Add(segments.Where(s => s.Length == 2).First(), "1");
+        digits.Add(segments.Where(s => s.Length == 3).First(), "7");
+        digits.Add(segments.Where(s => s.Length == 4).First(), "4");
+        digits.Add(segments.Where(s => s.Length == 7).First(), "8");
+
+        var fives = segments.Where(s => s.Length == 5).ToList();
+        var sixes = segments.Where(s => s.Length == 6).ToList();
+
+        var chars1 = digits.Where(kv => kv.Value == "1").First().Key.ToArray();
+        var three = fives.Where(f => chars1.All(c => f.Contains(c))).First();
+        digits.Add(three, "3");
+        fives.Remove(three);
+
+        var chars3 = three.ToArray();
+        var nine = sixes.Where(f => chars3.All(c => f.Contains(c))).First();
+        digits.Add(nine, "9");
+        sixes.Remove(nine);
+
+        var chars8 = digits.Where(kv => kv.Value == "8").First().Key.ToList();
+        var chars9 = nine.ToList();
+        chars9.ForEach(c => chars8.Remove(c));
+        var leftBottom = chars8.First();
+
+        var five = fives.Where(f => !f.Contains(leftBottom)).First();
+        var two = fives.Where(f => f.Contains(leftBottom)).First();
+        digits.Add(five, "5");
+        digits.Add(two, "2");
+
+        var zero = sixes.Where(f => chars1.All(c => f.Contains(c))).First();
+        var six = sixes.Where(f => f != zero).First();
+
+        digits.Add(six, "6");
+        digits.Add(zero, "0");
+
+        var solution = "";
+
+        foreach (var num in numbers)
+        {
+            solution += digits[num];
+        }
+
+        return int.Parse(solution);
+    }
+}
