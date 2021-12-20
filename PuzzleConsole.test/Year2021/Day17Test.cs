8cf403902229ad1c6967ccbb5d827aabd97a5414
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2021-12-20 19:11:26 +0100

    day 17 start

---
+++ PuzzleConsole.test/Year2021/Day17Test.cs
@@ -0,0 +1,17 @@
+﻿using PuzzleConsole.Year2021.Day17;
+
+namespace PuzzleConsole.test.Year2021;
+
+public partial class Day17Test
+{
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day17();
+
+        scenario.Fact("Sut should be puzzle", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2021/Day17.cs
@@ -0,0 +1,9 @@
+﻿namespace PuzzleConsole.Year2021.Day17;
+
+public class Day17 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+        return puzzle;
+    }
+}
