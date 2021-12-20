da1a6acceecc37cf485f02cd667098378265de44
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-12-20 17:14:24 +0100

    added day 1 2015

---
+++ PuzzleConsole.test/Year2015/Day1Test.cs
@@ -0,0 +1,41 @@
+﻿using PuzzleConsole.Year2015.Day1;
+using Xunit;
+
+namespace PuzzleConsole.test.Year2015;
+
+public class Day1Test
+{
+    [Fact]
+    public void TestCanSolve()
+    {
+        var mocker = new AutoMocker(Moq.MockBehavior.Strict);
+
+        var sut = mocker.CreateInstance<Day1>();
+
+        var puzzle = new string[]
+        {
+            "(())",
+        };
+
+        var solution = sut.Solve(puzzle);
+
+        solution.First().Should().Be("0");
+    }
+    [Fact]
+    public void TestCanSolvePart2()
+    {
+        var mocker = new AutoMocker(Moq.MockBehavior.Strict);
+
+        var sut = mocker.CreateInstance<Day1>();
+
+        var puzzle = new string[]
+        {
+            "()())",
+        };
+
+        var solution = sut.Solve(puzzle);
+
+        solution.Should().HaveCount(2);
+        solution.Last().Should().Be("5");
+    }
+}
