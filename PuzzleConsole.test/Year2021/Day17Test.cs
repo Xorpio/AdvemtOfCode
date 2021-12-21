a3eb7acb3cd6d56fc079d3e166ecbdf66d1ffea0
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2021-12-21 16:16:59 +0100

    aded day 17

--- PuzzleConsole.test/Year2021/Day17Test.cs
+++ PuzzleConsole.test/Year2021/Day17Test.cs
@@ -13,5 +13,67 @@ public partial class Day17Test
         {
             sut.Should().NotBeNull();
         });
+
+        scenario.Fact("Resolve minmax", () =>
+        {
+            var input = "target area: x=20..30, y=-10..-5";
+
+            (int XMin, int XMax, int YMin, int YMax) expected = new(20, 30, -5, -10);
+
+            var solution = sut.GetMinMax(input);
+            solution.Should().Be(expected);
+        });
+
+        var testCases = new (int x, int y, bool hit)[]
+        {
+            new (7, 2, true),
+            new (6, 3, true),
+            new (6, 9, true),
+            new (9, 0, true),
+            new (17, -4, false),
+        };
+
+        foreach (var testCase in testCases)
+        {
+
+            scenario.Theory("CalcFire", testCase, () =>
+            {
+                (int XMin, int XMax, int YMin, int YMax) target = new(20, 30, -5, -10);
+
+                var solution = sut.Fire(testCase.x, testCase.y, target);
+                solution.Should().Be(testCase.hit);
+            });
+        }
+
+        scenario.Fact("Get max Y", () =>
+        {
+            var input = "target area: x=20..30, y=-10..-5";
+
+            (int XMin, int XMax, int YMin, int YMax) target = new(20, 30, -5, -10);
+            
+            var expected = 45;
+
+            var solution = sut.FindMaxYVelocity(target);
+
+            solution.maxY.Should().Be(expected);
+        });
+
+        var input = "target area: x=20..30, y=-10..-5";
+        var solution = sut.Solve(new string [] {input });
+
+        scenario.Fact("Sovle part 1", () =>
+        {
+
+            var expected = "45";
+
+            solution.First().Should().Be(expected);
+        });
+
+        scenario.Fact("Sovle part 2", () =>
+        {
+            var expected = "112";
+
+            solution.Last().Should().Be(expected);
+        });
     }
 }
--- PuzzleConsole/Year2021/Day17.cs
+++ PuzzleConsole/Year2021/Day17.cs
@@ -4,6 +4,103 @@ public class Day17 : ISolver
 {
     public string[] Solve(string[] puzzle)
     {
-        return puzzle;
+        var target = GetMinMax(puzzle[0]);
+
+        var maxY = FindMaxYVelocity(target);
+
+        var Hit = 0;
+        for (int y = target.YMax; y < Math.Abs(target.YMax); y++)
+        {
+            for (int x = 0; x <= target.XMax; x++)
+                Hit += (Fire(x, y, target)) ? 1 : 0;
+        }
+
+        return new string[] { maxY.maxY.ToString(), Hit.ToString() };
+    }
+
+    public (int XMin, int XMax, int YMin, int YMax) GetMinMax(string input)
+    {
+        //var input = "target area: x=20..30, y=-10..-5";
+        var split = input.Split(": ")[1].Split(", ");
+        var x = split[0]
+            .Split('=')[1]
+            .Split("..");
+        var y = split[1]
+            .Split('=')[1]
+            .Split("..");
+
+        return (
+            int.Parse(x[0]),
+            int.Parse(x[1]),
+            int.Parse(y[1]),
+            int.Parse(y[0])
+            );
+    }
+
+    public bool Fire(int xVelocity, int yVelocity, (int XMin, int XMax, int YMin, int YMax) target)
+    {
+        (int x, int y) pos = new(0, 0);
+
+        var mis = false;
+
+        while (!mis)
+        {
+            pos.x += xVelocity;
+            pos.y += yVelocity;
+            
+            if(xVelocity > 0)
+                xVelocity--;
+            if (xVelocity < 0)
+                xVelocity++;
+
+            yVelocity--;
+
+            if (pos.x >= target.XMin && pos.x <= target.XMax &&
+                pos.y <= target.YMin && pos.y >= target.YMax)
+            {
+                return true;
+            }
+
+            if (pos.x > target.XMax || pos.y < target.YMax)
+            {
+                mis = true;
+            }
+        }
+
+        return false;
+    }
+
+    public (int maxY, int yVelocity) FindMaxYVelocity((int XMin, int XMax, int YMin, int YMax) target)
+    {
+        var yVelocity = Math.Abs(target.YMax) - 1;
+        return new(FireY(yVelocity, target.YMin, target.YMax).maxY, yVelocity);
+    }
+    public (int maxY, bool result) FireY(int yVelocity, int YMin, int YMax)
+    {
+        var pos = 0;
+        var max = 0;
+
+        var mis = false;
+
+        while (!mis)
+        {
+            pos += yVelocity;
+            if (pos > max)
+                max = pos;
+            
+            yVelocity--;
+
+            if (pos <= YMin && pos >= YMax)
+            {
+                return (max, true);
+            }
+
+            if (pos < YMax)
+            {
+                mis = true;
+            }
+        }
+
+        return (max, false);
     }
 }
