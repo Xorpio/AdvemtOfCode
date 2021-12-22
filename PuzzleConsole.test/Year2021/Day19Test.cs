a720d9c2e1f8533c24aa3c8a45f713abddbd0e2d
Xorpio <3060868+Xorpio@users.noreply.github.com>
2021-12-22 19:43:09 +0100

    added day 19 base code

---
+++ PuzzleConsole.test/Year2021/Day19Test.cs
@@ -0,0 +1,17 @@
+﻿using PuzzleConsole.Year2021.Day19;
+
+namespace PuzzleConsole.test.Year2021;
+
+public partial class Day19Test
+{
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day19();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2021/Day19.cs
@@ -0,0 +1,9 @@
+namespace PuzzleConsole.Year2021.Day19;
+
+public class Day19 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+	    return puzzle;
+    }
+}
