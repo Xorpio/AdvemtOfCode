86ea265da4442f66a784af732efc15961af96ac1
Xorpio <3060868+Xorpio@users.noreply.github.com>
2021-12-30 18:26:54 +0100

    added begin of day 23

---
+++ PuzzleConsole.test/Year2021/Day23Test.cs
@@ -0,0 +1,18 @@
+﻿using PuzzleConsole.Year2021.Day23;
+
+namespace PuzzleConsole.test.Year2021;
+
+[Trait("Year", "2021")]
+[Trait("Day", "23")]
+public partial class Day23Test {
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day23();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2021/Day23.cs
@@ -0,0 +1,9 @@
+namespace PuzzleConsole.Year2021.Day23;
+
+public class Day23 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+	    return puzzle;
+    }
+}
