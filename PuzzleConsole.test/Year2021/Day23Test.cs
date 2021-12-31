a64ae6a8ae3044983507f66219bfbcb56a71b06e
Niek de Gooijer <n.degooijer@it-workz.nl>
2021-12-31 13:57:03 +0100

    day 23

---
+++ PuzzleConsole.test/Year2021/Day23Test.cs
@@ -0,0 +1,194 @@
+﻿using PuzzleConsole.Year2021.Day23;
+
+namespace PuzzleConsole.test.Year2021;
+
+[Trait("Year", "2021")]
+[Trait("Day", "23")]
+public partial class Day23Test {
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day23();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+
+        var puzzle = new[]
+        {
+            "#############",
+            "#...........#",
+            "###B#C#B#D###",
+            "  #A#D#C#A#",
+            "  #########",
+        };
+
+        scenario.Fact("Puzzle should be created", () =>
+        {
+            var game = sut.PuzzleToGame(puzzle);
+
+            game.Amphipods.Should().HaveCount(8);
+            game.Amphipods[0].Type.Should().Be(PodType.Bronze);
+            game.Amphipods[0].Location.Col.Should().Be(2);
+            game.Amphipods[0].Location.Row.Should().Be(1);
+
+            game.Amphipods[1].Type.Should().Be(PodType.Copper);
+            game.Amphipods[1].Location.Col.Should().Be(4);
+            game.Amphipods[1].Location.Row.Should().Be(1);
+
+            game.Amphipods[2].Type.Should().Be(PodType.Bronze);
+            game.Amphipods[2].Location.Col.Should().Be(6);
+            game.Amphipods[2].Location.Row.Should().Be(1);
+
+            game.Amphipods[3].Type.Should().Be(PodType.Dessert);
+            game.Amphipods[3].Location.Col.Should().Be(8);
+            game.Amphipods[3].Location.Row.Should().Be(1);
+
+            game.Amphipods[4].Type.Should().Be(PodType.Amber);
+            game.Amphipods[4].Location.Col.Should().Be(2);
+            game.Amphipods[4].Location.Row.Should().Be(2);
+
+            game.Amphipods[5].Type.Should().Be(PodType.Dessert);
+            game.Amphipods[5].Location.Col.Should().Be(4);
+            game.Amphipods[5].Location.Row.Should().Be(2);
+
+            game.Amphipods[6].Type.Should().Be(PodType.Copper);
+            game.Amphipods[6].Location.Col.Should().Be(6);
+            game.Amphipods[6].Location.Row.Should().Be(2);
+
+            game.Amphipods[7].Type.Should().Be(PodType.Amber);
+            game.Amphipods[7].Location.Col.Should().Be(8);
+            game.Amphipods[7].Location.Row.Should().Be(2);
+
+        });
+
+        scenario.Fact("Solve should give solution", () =>
+        {
+            var game = sut.PuzzleToGame(puzzle);
+
+            var solution = sut.SolveGame(game, new List<Move>());
+
+            solution.Should().Be(12521);
+        });
+
+        scenario.Fact("Moves should be calculated to the left", () =>
+        {
+            var game = new Game(new List<Amphipod>()
+            {
+                new Amphipod(false, false, new Location(3, 0), PodType.Amber)
+            }, 0);
+
+            var start = new Location(2, 1);
+            var pod = new Amphipod(false, false, start, PodType.Dessert);
+
+            var expexted = new List<Move>()
+            {
+                new Move(start, new Location(0,0), PodType.Dessert),
+                new Move(start, new Location(1,0), PodType.Dessert)
+            };
+
+            var moves = pod.FindMoves(game);
+            moves.Should().BeEquivalentTo(expexted);
+        });
+
+        scenario.Fact("Moves should be calculated to the right", () =>
+        {
+            var game = new Game(new List<Amphipod>()
+            {
+                new Amphipod(false, false, new Location(1, 0), PodType.Amber),
+                new Amphipod(false, false, new Location(5, 0), PodType.Amber)
+            }, 0);
+
+            var start = new Location(2, 1);
+            var pod = new Amphipod(false, false, start, PodType.Dessert);
+
+            var expexted = new List<Move>()
+            {
+                new Move(start, new Location(3,0), PodType.Dessert),
+            };
+
+            var moves = pod.FindMoves(game);
+            moves.Should().BeEquivalentTo(expexted);
+        });
+
+        scenario.Fact("in hall should not calculate to hall", () =>
+        {
+            var game = new Game(new List<Amphipod>()
+            {
+                new Amphipod(false, false, new Location(5, 0), PodType.Amber)
+            }, 0);
+
+            var start = new Location(3, 0);
+            var pod = new Amphipod(true, false, start, PodType.Dessert);
+
+            var expexted = new List<Move>()
+            {
+            };
+
+            var moves = pod.FindMoves(game);
+            moves.Should().BeEquivalentTo(expexted);
+        });
+
+        scenario.Fact("In hall and to cave is free", () =>
+        {
+            var game = new Game(new List<Amphipod>()
+            {
+                new Amphipod(false, false, new Location(1, 0), PodType.Amber)
+            }, 0);
+
+            var start = new Location(3, 0);
+            var pod = new Amphipod(true, false, start, PodType.Dessert);
+
+            var expexted = new List<Move>()
+            {
+                new Move(start, new Location(8,1), PodType.Dessert),
+            };
+
+            var moves = pod.FindMoves(game);
+            moves.Should().BeEquivalentTo(expexted);
+        });
+
+        for (int i = 1; i < 3; i++)
+        {
+            scenario.Theory("In hall and to cave is free but cave is ocupied", i, () =>
+            {
+                var game = new Game(new List<Amphipod>()
+                {
+                    new Amphipod(false, false, new Location(1, 0), PodType.Amber),
+                    new Amphipod(false, false, new Location(8, i), PodType.Amber)
+                }, 0);
+
+                var start = new Location(3, 0);
+                var pod = new Amphipod(true, false, start, PodType.Dessert);
+
+                var expexted = new List<Move>()
+                {
+                };
+
+                var moves = pod.FindMoves(game);
+                moves.Should().BeEquivalentTo(expexted);
+            });
+        }
+
+        scenario.Fact("In hall and to cave is free but cave is ocupied by same type", () =>
+        {
+            var game = new Game(new List<Amphipod>()
+            {
+                new Amphipod(false, false, new Location(1, 0), PodType.Amber),
+                new Amphipod(false, false, new Location(8, 1), PodType.Dessert)
+            }, 0);
+
+            var start = new Location(3, 0);
+            var pod = new Amphipod(true, false, start, PodType.Dessert);
+
+            var expexted = new List<Move>()
+            {
+                new Move(start, new Location(8,1), PodType.Dessert),
+            };
+
+            var moves = pod.FindMoves(game);
+            moves.Should().BeEquivalentTo(expexted);
+        });
+    }
+}
---
+++ PuzzleConsole/Year2021/Day23.cs
@@ -0,0 +1,198 @@
+using PuzzleConsole.Year2021.Day22;
+
+namespace PuzzleConsole.Year2021.Day23;
+
+public class Day23 : ISolver
+{
+    public static readonly int[] Caves = { 2,4,6,8 };
+
+    public string[] Solve(string[] puzzle)
+    {
+        return puzzle;
+    }
+
+    public Game PuzzleToGame(string[] puzzle)
+    {
+        List<Amphipod> pods = new List<Amphipod>();
+
+        Location loc;
+        PodType type;
+
+        //#############
+        //#...........#
+        //###B#C#B#D###
+        //  #A#D#C#A#
+        //  #########
+
+        var line = puzzle[2];
+        var col = 3;
+        loc = new Location(col - 1, 1);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        col += 2;
+        loc = new Location(col - 1, 1);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        col += 2;
+        loc = new Location(col - 1, 1);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        col += 2;
+        loc = new Location(col - 1, 1);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        line = puzzle[3];
+        col = 3;
+        loc = new Location(col - 1, 2);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        col += 2;
+        loc = new Location(col - 1, 2);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        col += 2;
+        loc = new Location(col - 1, 2);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        col += 2;
+        loc = new Location(col - 1, 2);
+        type = PodTypeFromChar(line[col]);
+        pods.Add(new Amphipod(false, false, loc, type));
+
+        return new Game(pods, 0);
+    }
+
+    public PodType PodTypeFromChar(char c) => c switch
+    {
+        'A' => PodType.Amber,
+        'B' => PodType.Bronze,
+        'C' => PodType.Copper,
+        'D' => PodType.Dessert,
+        _ => throw new NotImplementedException()
+    };
+
+    public int SolveGame(Game game, List<Move> moves)
+    {
+        if (game.Amphipods.All(p => p.MovedToCave))
+        {
+            return moves.Sum(m => m.Score);
+        }
+
+        var possibleMoves = new List<Move>();
+        //vind alle mogelijke acties
+        foreach (var pod in game.Amphipods)
+        {
+            possibleMoves.AddRange(pod.FindMoves(game));
+        }
+
+        var score = int.MaxValue;
+
+        foreach (var move in  possibleMoves)
+        {
+            var pods = new List<Amphipod>(game.Amphipods);
+
+            var p = pods.Find(p => p.Location == move.From);
+            pods.Remove(p);
+            pods.Add(p with
+            {
+                Location = move.to,
+                MovedToHall = true,
+                MovedToCave = move.to.Row > 0
+            });
+
+            var m = new List<Move>(moves);
+            m.Add(move);
+            var newGame = game with { Amphipods = pods };
+
+            var newScore = SolveGame(newGame, m);
+            score = newScore > score ? score : newScore;
+        }
+
+        return score;
+    }
+}
+
+public record Game (List<Amphipod> Amphipods, int Score);
+
+public record Amphipod(bool MovedToHall, bool MovedToCave, Location Location, PodType Type)
+{
+    public IEnumerable<Move> FindMoves(Game game)
+    {
+        var moves = new List<Move>();
+        if (Location.Col != (int)Type && !game.Amphipods.Any(p => p.Location.Col == (int)Type && p.Type != Type)) // and cave is empty from others
+        {
+            if (!MovedToHall && (Location.Row == 1 || !game.Amphipods.Any(p => p.Location.Row == 1 && p.Location.Col == Location.Col)))
+            {
+                //try moving left
+                for (var i = Location.Col; i >= 0; i--)
+                {
+                    if (game.Amphipods.Any(p => p.Location.Col == i && p.Location.Row == 0)) // taken
+                    {
+                        i = -1;
+                    }
+                    else if (!Day23.Caves.Contains(i))
+                    {
+                        moves.Add(new Move(Location, new Location(i, 0), Type));
+                    }
+                }
+                //try moving Right
+                for (var i = Location.Col; i <= 10; i++)
+                {
+                    if (game.Amphipods.Any(p => p.Location.Col == i && p.Location.Row == 0)) // taken
+                    {
+                        i = 11;
+                    }
+                    else if (!Day23.Caves.Contains(i))
+                    {
+                        moves.Add(new Move(Location, new Location(i, 0), Type));
+                    }
+                }
+            }
+            else if (!MovedToCave && MovedToHall)
+            {
+                if (Location.Col > (int)Type &&  //move to left
+                    !game.Amphipods.Any(p => p.Location.Row == 0 && p.Location.Col.Between((int)Type, Location.Col)) && //and the way is free
+                    !game.Amphipods.Any(p => p.Location.Col == (int)Type && p.Type != Type)) // and cave is empty from others
+                {
+                    moves.Add(new Move(Location, new Location((int)Type, 1), Type));
+                }
+                if (Location.Col < (int)Type &&  //move to left
+                    !game.Amphipods.Any(p => p.Location.Row == 0 && p.Location.Col.Between(Location.Col, (int)Type)) && //and the way is free
+                    !game.Amphipods.Any(p => p.Location.Col == (int)Type && p.Type != Type)) // and cave is empty from others
+                {
+                    moves.Add(new Move(Location, new Location((int)Type, 1), Type));
+                }
+            }
+        }
+
+        return moves;
+    }
+}
+
+public record Location (int Col, int Row);
+
+public record Move(Location From, Location to, PodType Type)
+{
+    public int Score
+    {
+        get
+        {
+            return 1;
+        }
+    }
+};
+
+public enum PodType
+{
+    Amber = 2,
+    Bronze = 4,
+    Copper = 6,
+    Dessert = 8,
+}
