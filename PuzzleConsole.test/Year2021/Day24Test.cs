8dae93f79ea94f951cbbec4a29af702aa26609ec
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2022-01-02 13:48:17 +0100

    Added day 24 basis

---
+++ PuzzleConsole.test/Year2021/Day24Test.cs
@@ -0,0 +1,18 @@
+﻿using PuzzleConsole.Year2021.Day24;
+
+namespace PuzzleConsole.test.Year2021;
+
+[Trait("Year", "2021")]
+[Trait("Day", "24")]
+public partial class Day24Test {
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day24();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2021/Day24.cs
@@ -0,0 +1,9 @@
+namespace PuzzleConsole.Year2021.Day24;
+
+public class Day24 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+	    return puzzle;
+    }
+}
