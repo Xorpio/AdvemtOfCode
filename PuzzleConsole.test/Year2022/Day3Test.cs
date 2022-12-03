68e177171a822e608aaae0928d95a38f80a2b4a1
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2022-12-03 17:44:09 +0100

    Adding day3

--- PuzzleConsole.test/Year2022/Day3Test.cs
+++ PuzzleConsole.test/Year2022/Day3Test.cs
@@ -2,10 +2,10 @@ using PuzzleConsole.Year2022.Day3;
 
 namespace PuzzleConsole.test.Year2022;
 
-public partial class Day3Test
+public partial class Day3Tests
 {
     [Scenario]
-    public void SolveTest(ScenarioContext scenario)
+    public void SolveTests(ScenarioContext scenario)
     {
         var sut = new Day3();
 
@@ -13,5 +13,39 @@ public partial class Day3Test
         {
             sut.Should().NotBeNull();
         });
+
+        var puzzles = new (string p, int s1, int s2)[]
+        {
+            ("aa", 1, 0),
+            ("zz", 26, 0),
+            ("AA", 27, 0),
+            ("ZZ", 52, 0),
+            ("vJrwpWtwJgWrhcsFMMfFFhFp", 16, 0),
+            ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", 38, 0),
+            ("PmmdzqPrVvPwwTWBwg", 42, 0),
+            ("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", 22, 0),
+            ("ttgJtRGJQctTZtZT", 20, 0),
+            ("CrZsJsPPZsGzwwsLwLmpwMDw", 19, 0),
+            ("""
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+            """
+                , 157, 70),
+        };
+
+        foreach (var p in puzzles)
+        {
+            scenario.Theory("Puzzle solve", p, () =>
+            {
+                var puzzleLines = p.p.Split("\r\n");
+
+                sut.Solve(puzzleLines)[0].Should().Be(p.s1.ToString());
+                sut.Solve(puzzleLines)[1].Should().Be(p.s2.ToString());
+            });
+        }
     }
 }
