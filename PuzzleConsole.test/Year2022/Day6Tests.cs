603b03a4ce91441f50c95652a47a33231507d9ce
Niek de Gooijer <n.degooijer@it-workz.nl>
2022-12-06 06:29:55 +0100

    Part 2

--- PuzzleConsole.test/Year2022/Day6Tests.cs
+++ PuzzleConsole.test/Year2022/Day6Tests.cs
@@ -16,20 +16,24 @@ public partial class Day6Tests
             sut.Should().NotBeNull();
         });
 
-        var puzzles = new (string Puzzle, int Answer)[]
+        var puzzles = new (string Puzzle, int AnswerPart1, int AnswerPart2)[]
         {
-            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-            ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 6,23 ),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11,26),
         };
 
         foreach (var p in puzzles)
         {
-            scenario.Theory("Puzzle should be solved", p, () =>
+            scenario.Theory("Puzzle part 1 should be solved", p, () =>
             {
-                sut.Solve(new[] { p.Puzzle })[0].Should().Be(p.Answer.ToString());
+                sut.Solve(new[] { p.Puzzle })[0].Should().Be(p.AnswerPart1.ToString());
+            });
+            scenario.Theory("Puzzle part 2 should be solved", p, () =>
+            {
+                sut.Solve(new[] { p.Puzzle })[1].Should().Be(p.AnswerPart2.ToString());
             });
         }
     }
--- PuzzleConsole/Year2022/Day6.cs
+++ PuzzleConsole/Year2022/Day6.cs
@@ -4,22 +4,31 @@ public class Day6 : ISolver
 {
     public string[] Solve(string[] puzzle)
     {
+        var answer1 = 0;
+        var answer2 = 0;
         var line = puzzle[0];
         for (var index = 3; index < line.Length; index++)
         {
-            var arr = new char[]
-            {
-                line[index],
-                line[index - 1],
-                line[index - 2],
-                line[index - 3],
-            };
+            var arr = line.Skip(index - 3)
+                .Take(4);
             if (arr.Distinct().Count() > 3)
             {
-                return new[] { (index + 1).ToString() };
+                answer1 = (index + 1);
+                break;
             }
         }
 
-        throw new Exception("No answer found");
+        for (var index = 13; index < line.Length; index++)
+        {
+            var arr = line.Skip(index - 13)
+                .Take(14);
+            if (arr.Distinct().Count() > 13)
+            {
+                answer2 = (index + 1);
+                break;
+            }
+        }
+
+        return new[] { (answer1).ToString(), answer2.ToString() };
     }
 }
