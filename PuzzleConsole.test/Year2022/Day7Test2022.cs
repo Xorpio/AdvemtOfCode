5bb1fff4ffdfab4d8e1c4d2729d5df1dbf3c37e9
Niek de Gooijer <n.degooijer@it-workz.nl>
2022-12-07 06:43:12 +0100

    Day 7 begin

---
+++ PuzzleConsole.test/Year2022/Day7Test2022.cs
@@ -0,0 +1,19 @@
+﻿using PuzzleConsole.Year2022.Day7;
+
+namespace PuzzleConsole.test.Year2022;
+
+[Trait("Year", "2022")]
+[Trait("Day", "7")]
+public partial class Day7Test2022
+{
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day7();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2022/Day7.cs
@@ -0,0 +1,9 @@
+namespace PuzzleConsole.Year2022.Day7;
+
+public class Day7 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+	    return puzzle;
+    }
+}
