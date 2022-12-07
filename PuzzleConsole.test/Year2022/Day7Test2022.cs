564a903311587b54883491db8eeb84a20ff5863b
Niek de Gooijer <n.degooijer@it-workz.nl>
2022-12-07 08:54:46 +0100

    Day 7 2022

--- PuzzleConsole.test/Year2022/Day7Test2022.cs
+++ PuzzleConsole.test/Year2022/Day7Test2022.cs
@@ -15,5 +15,42 @@ public partial class Day7Test2022
         {
             sut.Should().NotBeNull();
         });
+
+        var sampleInput = """
+        $ cd /
+        $ ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        $ cd a
+        $ ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        $ cd e
+        $ ls
+        584 i
+        $ cd ..
+        $ cd ..
+        $ cd d
+        $ ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k
+        """;
+        var lines = sampleInput.Split("\n");
+
+        scenario.Fact("Solve should have result part 1", () =>
+        {
+            sut.Solve(lines)[0].Should().Be("95437");
+        });
+
+        scenario.Fact("Solve should have result part 2", () =>
+        {
+            sut.Solve(lines)[1].Should().Be("24933642");
+        });
     }
 }
--- PuzzleConsole/Year2022/Day7.cs
+++ PuzzleConsole/Year2022/Day7.cs
@@ -1,9 +1,96 @@
+using System.Runtime.InteropServices.ComTypes;
+using Spectre.Console.Rendering;
+
 namespace PuzzleConsole.Year2022.Day7;
 
 public class Day7 : ISolver
 {
     public string[] Solve(string[] puzzle)
     {
-	    return puzzle;
+        puzzle = puzzle.Select(p => p.Trim()).ToArray();
+
+        var dir = ToDir(puzzle);
+
+        foreach (var p1 in dir)
+        {
+            foreach (var p2 in dir
+                         .Where(p2 => p1.Key != p2.Key)
+                         .Where(p2 => p2.Key.StartsWith(p1.Key)))
+            {
+                dir[p1.Key] += p2.Value;
+
+            }
+        }
+
+        var answer = dir.Where(d => d.Value <= 100_000)
+              .Select(d => d.Value)
+              .Sum();
+
+        //part 2
+        var totalSpace = 70_000_000;
+        var neededSpace = 30_000_000;
+
+        string root = Path.GetFullPath("/");
+
+        var freeSpace = (totalSpace - dir[root]);
+        var toDelete = neededSpace - freeSpace;
+
+        var answer2 = dir
+            .Where(d => d.Value > toDelete)
+            .OrderBy(d => d.Value)
+            .First().Value;
+
+
+        return new [] { answer.ToString(), answer2.ToString() };
+    }
+
+    public Dictionary<string, decimal> ToDir(string[] puzzle)
+    {
+        var path = Path.GetFullPath("/");
+
+        var directories = new Dictionary<string, decimal>();
+
+        var listing = false;
+        decimal totalSize = 0;
+        foreach (var line in puzzle)
+        {
+            switch ((listing, line.Split(' ')))
+            {
+                case (_, ["$", "cd", string location]):
+                    if (listing)
+                    {
+                        directories.TryAdd(path, totalSize);
+                        totalSize = 0;
+                        listing = false;
+                    }
+
+                    path = Path.GetFullPath(Path.Combine(path, location));
+                    break;
+
+                case (_, ["$", "ls"]):
+                    listing = true;
+                    break;
+
+                case (true, ["dir", _]):
+                    continue;
+                    break;
+
+                case (true, [var size, var name]):
+                    totalSize += decimal.Parse(size);
+                    break;
+
+                default:
+                    throw new Exception($"{line} not supported");
+            }
+        }
+
+        if (listing)
+        {
+            directories.TryAdd(path, totalSize);
+            totalSize = 0;
+            listing = false;
+        }
+
+        return directories;
     }
 }
