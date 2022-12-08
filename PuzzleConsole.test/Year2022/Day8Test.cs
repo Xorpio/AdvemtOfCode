279277c0fdd201135b37ca207734daea172119b5
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2022-12-08 06:31:18 +0100

    Toeveogen day 8 2022

---
+++ PuzzleConsole.test/Year2022/Day8Test.cs
@@ -0,0 +1,19 @@
+﻿using PuzzleConsole.Year2022.Day8;
+
+namespace PuzzleConsole.test.Year2022;
+
+[Trait("Year", "2022")]
+[Trait("Day", "8")]
+public partial class Day8Test
+{
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day8();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2022/Day8.cs
@@ -0,0 +1,9 @@
+namespace PuzzleConsole.Year2022.Day8;
+
+public class Day8 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+	    return puzzle;
+    }
+}
