cf5dfa0aea4a3d7da95df48b4f74e3c4908c5c21
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2022-12-08 07:00:23 +0100

    Part 1 dag 8 2022

---
+++ PuzzleConsole.test/Year2022/Day8Test2022.cs
@@ -0,0 +1,33 @@
+﻿using PuzzleConsole.Year2022.Day8;
+
+namespace PuzzleConsole.test.Year2022;
+
+[Trait("Year", "2022")]
+[Trait("Day", "8")]
+public partial class Day8Test2022
+{
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day8();
+
+        var sampleInput = """
+        30373
+        25512
+        65332
+        33549
+        35390
+        """;
+        var lines = sampleInput.Split("\n").Select(s => s.Trim()).ToArray();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+
+        scenario.Fact("Solve moet part 1", () =>
+        {
+            int.Parse(sut.Solve(lines)[0]).Should().Be(21);
+        });
+    }
+}
--- PuzzleConsole/Year2022/Day8.cs
+++ PuzzleConsole/Year2022/Day8.cs
@@ -4,6 +4,88 @@ public class Day8 : ISolver
 {
     public string[] Solve(string[] puzzle)
     {
-	    return puzzle;
+        var grid = new int[puzzle[0].Length, puzzle.Count()];
+
+        for (var y = 0; y < puzzle.Length; y++)
+        {
+            var line = puzzle[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                var c = line[x];
+                var n = int.Parse(c.ToString());
+
+                grid[x, y] = n;
+            }
+        }
+
+        var visible = 0;
+        for (var x = 0; x < grid.GetLength(0); x++)
+        for (var y = 0; y < grid.GetLength(1); y++)
+        {
+            if (fromNorth(x, grid, y) ||
+                fromEast(x, grid, y) ||
+                fromSouth(x, grid, y) ||
+                fromWest(x, grid, y))
+            {
+                visible++;
+            }
+        }
+
+        return new string[]
+        {
+            visible.ToString()
+        };
+    }
+
+    private bool fromNorth(int x, int[,] grid, int y)
+    {
+        for (var xx = 0; xx < x; xx++)
+        {
+            if (grid[xx, y] >= grid[x, y])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool fromEast(int x, int[,] grid, int y)
+    {
+        for (var yy = grid.GetLength(1) - 1; yy > y; yy--)
+        {
+            if (grid[x, yy] >= grid[x, y])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool fromSouth(int x, int[,] grid, int y)
+    {
+        for (var xx = grid.GetLength(0) - 1; xx > x; xx--)
+        {
+            if (grid[xx, y] >= grid[x, y])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool fromWest(int x, int[,] grid, int y)
+    {
+        for (var yy = 0; yy < y; yy++)
+        {
+            if (grid[x, yy] >= grid[x, y])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
