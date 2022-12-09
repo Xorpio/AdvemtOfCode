71180361274198b3e9a371eb33588c68212f13fb
Niek de Gooijer <n.degooijer@it-workz.nl>
2022-12-09 07:03:06 +0100

    basis voor dag 9

---
+++ PuzzleConsole.test/Year2022/Day9Test2022.cs
@@ -0,0 +1,19 @@
+﻿using PuzzleConsole.Year2022.Day9;
+
+namespace PuzzleConsole.test.Year2022;
+
+[Trait("Year", "2022")]
+[Trait("Day", "9")]
+public partial class Day9Test2022
+{
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day9();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+    }
+}
---
+++ PuzzleConsole/Year2022/Day9.cs
@@ -0,0 +1,9 @@
+namespace PuzzleConsole.Year2022.Day9;
+
+public class Day9 : ISolver
+{
+    public string[] Solve(string[] puzzle)
+    {
+	    return puzzle;
+    }
+}
