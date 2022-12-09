2ebbb832479c7483fa1ceaa4d4ccd27b52e58408
Niek de Gooijer <n.degooijer@it-workz.nl>
2022-12-09 07:26:41 +0100

    part 1

--- PuzzleConsole/Year2022/Day9.cs
+++ PuzzleConsole/Year2022/Day9.cs
@@ -1,9 +1,94 @@
+using System.Globalization;
+
 namespace PuzzleConsole.Year2022.Day9;
 
 public class Day9 : ISolver
 {
     public string[] Solve(string[] puzzle)
     {
-	    return puzzle;
+        var trail = new HashSet<Coord>();
+
+        var head = new Coord(0, 0);
+        var tail = head;
+
+        trail.Add(tail);
+
+        foreach (var instruction in puzzle)
+        {
+            var split = instruction.Split(' ');
+            var directions = split[0];
+            var ammount = int.Parse(split[1]);
+
+            switch (directions)
+            {
+                case "R":
+                    while (ammount > 0)
+                    {
+                        head = head with { X = head.X + 1 };
+                        tail = FollowHead(head, tail);
+                        trail.Add(tail);
+                        ammount--;
+                    }
+                    break;
+
+                case "U":
+                    while (ammount > 0)
+                    {
+                        head = head with { Y = head.Y + 1 };
+                        tail = FollowHead(head, tail);
+                        trail.Add(tail);
+                        ammount--;
+                    }
+                    break;
+
+                case "D":
+                    while (ammount > 0)
+                    {
+                        head = head with { Y = head.Y - 1 };
+                        tail = FollowHead(head, tail);
+                        trail.Add(tail);
+                        ammount--;
+                    }
+                    break;
+
+                case "L":
+                    while (ammount > 0)
+                    {
+                        head = head with { X = head.X - 1 };
+                        tail = FollowHead(head, tail);
+                        trail.Add(tail);
+                        ammount--;
+                    }
+                    break;
+
+                default:
+                    throw new Exception($"Case {instruction}");
+            }
+        }
+
+        return new[] { trail.Count().ToString() };
+    }
+
+    private Coord FollowHead(Coord head, Coord tail)
+    {
+        if (head.X > tail.X + 1)
+        {
+            return head with { X = head.X - 1 };
+        }
+        if (head.X < tail.X - 1)
+        {
+            return head with { X = head.X + 1 };
+        }
+        if (head.Y > tail.Y + 1)
+        {
+            return head with { Y = head.Y - 1 };
+        }
+        if (head.Y < tail.Y - 1)
+        {
+            return head with { Y = head.Y + 1 };
+        }
+        return tail;
     }
 }
+
+public record Coord (int X, int Y);
