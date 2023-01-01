bcff9ea122cabfd9c4553af3c30191628f507654
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-01-01 17:58:52 +0100

    Day 24 part 1

---
+++ PuzzleConsole.test/Year2022/Day24Test2022.cs
@@ -0,0 +1,33 @@
+﻿using PuzzleConsole.Year2022.Day24;
+
+namespace PuzzleConsole.test.Year2022;
+
+[Trait("Year", "2022")]
+[Trait("Day", "24")]
+public partial class Day24Test2022 {
+    [Scenario]
+    public void SolveTest(ScenarioContext scenario)
+    {
+        var sut = new Day24();
+
+        var sampleInput = """
+        #.######
+        #>>.<^<#
+        #.<..<<#
+        #>v.><>#
+        #<^v^^>#
+        ######.#
+        """;
+        var lines = sampleInput.Split("\n").Select(s => s.Trim()).ToArray();
+
+        scenario.Fact("Sut should not be null", () =>
+        {
+            sut.Should().NotBeNull();
+        });
+
+        scenario.Fact("Small sample test", () =>
+        {
+            int.Parse(sut.Solve(lines)[0]).Should().Be(18);
+        });
+    }
+}
