79652e1f70861815e6cb944547e5fc19db9582d3
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-01-02 21:40:46 +0100

    day 16 part 2

--- PuzzleConsole/Year2022/Day16.cs
+++ PuzzleConsole/Year2022/Day16.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics.Tracing;
+using System.Security;
+
 namespace PuzzleConsole.Year2022.Day16;
 
 public class Day16 : ISolver
@@ -32,210 +35,212 @@ public class Day16 : ISolver
             });
         }
 
-        var paths = new Queue<(string me, string? meTar, string el, string? elTar, string[] open, int time, int flowed)>();
-        paths.Enqueue(("AA", null, "AA", null, Array.Empty<string>(), 0, 0));
-
-        var currentMax = 0;
-
-        var sw = new System.Diagnostics.Stopwatch();
-        for (int minutes = 1; minutes <= 26; minutes++)
-        {
-            sw.Reset();
-            sw.Start();
-            while(paths.Count > 0 && paths.Peek().time == minutes - 1)
-            {
-                var p = paths.Dequeue();
-
-                if (!shouldContinue(p, currentMax, valves))
-                {
-                    continue;
-                }
-
-                var others = paths.Where(_p => _p.time >= p.time
-                    && _p.elTar == p.meTar
-                    && _p.meTar == p.elTar
-                    && _p.me == p.el
-                    && _p.el == p.me
-                    && _p.flowed >= p.flowed
-                    && _p.open.Except(p.open).Any()
-                );
-                if (others.Any())
-                {
-                    continue;
-                }
-
-                if (valves.Where(v => v.flowRate > 0).All(v => p.open.Contains(v.Name)))
-                {
-                    var timeLeft = 26 - p.time;
-                    var max = valves
-                        .Select(v => v.flowRate * timeLeft)
-                        .Sum() + p.flowed;
-
-                    if (max > currentMax)
-                        currentMax = max;
-
-                    Console.WriteLine($"Found max {max} (1 of {paths.Count}");
-                    continue;
-                }
-
-                //count opened
-                p.flowed += valves.Where(v => p.open.Contains(v.Name))
-                    .Select(v => v.flowRate)
-                    .Sum();
-
-                if (p.flowed > currentMax)
-                    currentMax = p.flowed;
-
-                p.time = minutes;
-
-
-                if (p.meTar == p.me && p.elTar == p.el)
-                {
-                    var open = p.open
-                        .Append(p.meTar)
-                        .Append(p.elTar)
-                        .ToArray();
-
-                    p.open = open;
-                    p.elTar = null;
-                    p.meTar = null;
-                }
-                else if (p.meTar == p.me)
-                {
-                    var open = p.open
-                        .Append(p.meTar)
-                        .ToArray();
-
-                    p.open = open;
-                    p.meTar = null;
-                }
-                else if (p.elTar == p.el)
-                {
-                    var open = p.open
-                        .Append(p.elTar)
-                        .ToArray();
-
-                    p.open = open;
-                    p.elTar = null;
-                }
-
-                var targets = valves.Where(v => !p.open.Contains(v.Name))
-                    .Where(v => v.flowRate > 0)
-                    .Select(v => v.Name).ToList();
-
-
-                //both elefant and me are not assigned
-                if (p.meTar is null && p.elTar is null)
-                {
-                    var list =  targets.SelectMany(tme => targets
-                        .Where(tel => tel != tme)
-                        .Select(tel => (tme, tel)));
-
-                    if (targets.Count() == 1)
-                    {
-                        if (GetPath(p.me, targets.First(), valves) >
-                            GetPath(p.el, targets.First(), valves))
-                        {
-                            p.meTar = "";
-                            p.elTar = targets.First();
-                        }
-                        else
-                        {
-                            p.elTar = "";
-                            p.meTar = targets.First();
-                        }
-
-                        p.me = findNext(p.me, p.meTar, valves);
-                        p.el = findNext(p.el, p.elTar, valves);
-                        paths.Enqueue(p);
-
-                        continue;
-                    }
-
-                    if (list.Count() == 0)
-                    {
-                        p.meTar = "";
-                        p.elTar = "";
-                        paths.Enqueue(p);
-                        continue;
-                    }
-
-                    foreach (var t in list)
-                    {
-                        var nextMe = findNext(p.me, t.tme, valves);
-                        var nextEl = findNext(p.el, t.tel, valves);
-
-                       paths.Enqueue((nextMe, t.tme, nextEl,t.tel, p.open, minutes, p.flowed));
-                    }
-
-                    continue;
-                }
-
-                if (p.meTar is null)
-                {
-                    var list = targets
-                            .Where(tme => p.elTar != tme);
-
-                    if (list.Count() == 0)
-                    {
-                        p.meTar = "";
-                        p.el = findNext(p.el, p.elTar, valves);
-                        paths.Enqueue(p);
-                        continue;
-                    }
-
-                    foreach (var t in list)
-                    {
-                        var nextMe = findNext(p.me, t, valves);
-                        var nextEl = findNext(p.el, p.elTar, valves);
-
-                       paths.Enqueue((nextMe, t, nextEl, p.elTar, p.open, minutes, p.flowed));
-                    }
-
-                    continue;
-                }
-
-                if (p.elTar is null)
-                {
-                    var list = targets
-                            .Where(tme => p.meTar != tme);
-
-                    if (list.Count() == 0)
-                    {
-                        p.elTar = "";
-                        p.me = findNext(p.me, p.meTar, valves);
-                        paths.Enqueue(p);
-                        continue;
-                    }
-
-                    foreach (var t in list)
-                    {
-                        var nextMe = findNext(p.me, p.meTar, valves);
-                        var nextEl = findNext(p.el, t, valves);
-
-                       paths.Enqueue((nextMe, p.meTar, nextEl, t, p.open, minutes, p.flowed));
-                    }
-
-                    continue;
-                }
-
-
-                p.me = findNext(p.me, p.meTar, valves);
-                p.el = findNext(p.el, p.elTar, valves);
-
-                paths.Enqueue(p);
-            }
-
-            sw.Stop();
-            Console.WriteLine($"minute {minutes} done. Found {paths.Count} paths in {sw.Elapsed}");
-        }
-
+        // var part2 = FindSolutionPart2(valves);
         var res = FindSolution(valves, 30, new List<string>(), "AA");
+        var part2 = FindSolutionPart2Recursive(new[]
+        {
+            (26, "AA"),
+            (26, "AA"),
+        }, valves.Where(v => v.flowRate > 0).Select(v => v.Name).ToArray(), valves);
+        Console.WriteLine("Found part 2");
 
         return new string[]
         {
-            res.ToString(), currentMax.ToString(),
+            res.ToString(),
+            part2.ToString()
         };
+    }
+
+    private int FindSolutionPart2Recursive((int TimeLeft, string position)[] players, string[] valvesToOpen, List<Valve> valves)
+    {
+        var p = players[0].TimeLeft > players[1].TimeLeft ? 0 : 1;
+
+        var best = 0;
+        foreach (var valve in valvesToOpen)
+        {
+            if (valvesToOpen.Length > 10)
+            {
+                // Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+                Console.WriteLine($"From: {players[p].position} with timeleft {players[p].TimeLeft} to {valve}. best: {best}");
+            }
+            var move = GetPath(players[p].position, valve, valves);
+
+            if (players[p].TimeLeft - (move + 1) > 0)
+            {
+
+                var ps = new (int TimeLeft, string position)[]
+                {
+                    players[0],
+                    players[1]
+                };
+                ps[p].TimeLeft -= (move + 1);
+                ps[p].position = valve;
+
+                var result = FindSolutionPart2Recursive(ps, valvesToOpen.Where(v => v != valve).ToArray(), valves)
+                                + (valves.Where(v => v.Name == valve).First().flowRate * ps[p].TimeLeft);
+
+                if (best < result)
+                    best = result;
+            }
+        }
+
+        return best;
+    }
+
+    private int openCounter = 0;
+
+    private string[] ToOpen()
+    {
+        var valves = new string[]
+        {
+            "JJ",
+            "DD",
+            "HH",
+            "BB",
+            "CC",
+            "EE"
+        };
+        return new[] { valves[openCounter++] };
+    }
+
+    private int FindSolutionPart2(List<Valve> valves)
+    {
+        var universes = new List<(
+            (string L, string? T, int M)[] Players,
+            string[] OpenValves,
+            int Time,
+            int Flowed
+            )>();
+
+        var toOpenValves = valves.Where(v => v.flowRate > 0)
+            .Select(v => v.Name);
+
+        universes.Add((new (string, string, int)[]
+        {
+            ("AA", null, 26),
+            ("AA", null, 26),
+        }, new string[]
+        {
+        }, 26, 0));
+
+        var max = 0;
+
+        var seen = new List<(string opened, int flowed)>();
+
+        ((string L, string T, int M)[] Players, string[] OpenValves, int Time, int Flowed) universe;
+        while (universes.Any())
+        {
+            var universeNoTarget = universes
+                .Where(u => u.Players[0].T == null || u.Players[1].T == null);
+
+
+            if (universeNoTarget.Any())
+            {
+                universe = universeNoTarget.First();
+                universes.Remove(universe);
+
+                var pIndex = (universe.Players[0].T == null) ? 0 : 1;
+                var p2 = universe.Players[pIndex == 0 ? 1 : 0];
+                var toOpen = toOpenValves.Where(v => !universe.OpenValves.Contains(v) && p2.T != v);
+
+                if (!toOpen.Any())
+                {
+                    var flowed =
+                        + universe.Flowed
+                        + valves.Where(v => universe.OpenValves.Contains(v.Name))
+                        .Select(v => v.flowRate)
+                        .Sum() * (universe.Time)
+                        + valves.Where(v => v.Name == p2.T)
+                            .Select( v => v.flowRate)
+                            .Sum() * (p2.M - 1)
+                        ;
+                    if (flowed > max)
+                        max = flowed;
+                    continue;
+                }
+
+                // toOpen = ToOpen();
+                foreach (var open in toOpen)
+                {
+                    var path = GetPath(universe.Players[pIndex].L, open, valves);
+                    var newPlayers = new (string L, string? T, int M)[]
+                    {
+                        universe.Players[pIndex],
+                        p2
+                    };
+                    newPlayers[0].M = universe.Time - path;
+                    newPlayers[0].T = open;
+
+                    // check for duplicates
+                    if (!ContainsUniverse(universes, newPlayers, universe, seen))
+                    {
+                        universes.Add((newPlayers, universe.OpenValves, universe.Time, universe.Flowed));
+                    }
+                    continue;
+                }
+            }
+            else
+            {
+                var universeToTarget = universes
+                        .OrderByDescending(u => u.Time)
+                        .ThenByDescending(u => u.Flowed)
+                    ;
+
+                universe = universeToTarget.First();
+                universes.Remove(universe);
+
+                //shortest first
+                universe.Players = universe.Players.OrderByDescending(p => p.M).ToArray();
+                universe.Players[0].L = universe.Players[0].T;
+                universe.Players[0].T = null;
+
+                if (universe.Time > universe.Players[0].M - 1)
+                {
+                    var time = universe.Time;
+                    universe.Time = universe.Players[0].M - 1;
+                    universe.Flowed += valves
+                        .Where(v => universe.OpenValves.Contains(v.Name))
+                        .Select(v => v.flowRate)
+                        .Sum() * (time - universe.Time);
+                }
+
+                var openValves = new List<string>(universe.OpenValves);
+                openValves.Add(universe.Players[0].L);
+                universe.OpenValves = openValves.ToArray();
+
+                var opened = string.Join('-', universe.OpenValves.OrderBy(u => u));
+                if (!seen.Where(s => s.opened == opened && s.flowed >= universe.Flowed).Any())
+                {
+                    universes.Add(universe);
+                }
+                else
+                {
+                    seen.Add((opened, universe.Flowed));
+                }
+            }
+
+            Console.SetCursorPosition(Console.GetCursorPosition().Top, 0);
+            Console.WriteLine($"{max} - {universes.Count}".PadRight(10, ' '));
+        }
+
+        return max;
+    }
+
+    private bool ContainsUniverse(
+        List<((string L, string T, int M)[] Players, string[] OpenValves, int Time, int Flowed)> universes,
+        (string L, string T, int M)[] newPlayers,
+        ((string L, string T, int M)[] Players, string[] OpenValves, int Time, int Flowed) universe,
+        List<(string opened, int flowed)> seen)
+    {
+        var us = universes.Where(u => Enumerable.SequenceEqual(u.OpenValves, universe.OpenValves));
+        us = us.Where(u => Enumerable.SequenceEqual(
+            u.Players.OrderBy(p => p.L).ThenBy(p => p.T).ThenBy(p => p.T),
+            newPlayers.OrderBy(p => p.L).ThenBy(p => p.T).ThenBy(p => p.T)));
+        us = us.Where(u => u.Time == universe.Time);
+        us = us.Where(u => u.Flowed == universe.Flowed);
+
+        var opened = string.Join('-', universe.OpenValves.OrderBy(u => u));
+        return us.Any() || seen.Where(s => s.opened == opened && s.flowed >= universe.Flowed).Any();
     }
 
     private bool shouldContinue((string me, string? meTar, string el, string? elTar, string[] open, int time, int flowed) solution, int currentMax, List<Valve> valves)
@@ -296,7 +301,7 @@ public class Day16 : ISolver
             {
                 var o = new List<string>(Opened);
                 o.Add(top.Name);
-                Console.WriteLine($"Checking in minute {minutes} for path: {string.Join("->", o)}");
+                // Console.WriteLine($"Checking in minute {minutes} for path: {string.Join("->", o)}");
                 var deeperSol = FindSolution(valves, minutes - (move + 1), o, top.Name);
                 solutions.Add(deeperSol + (top.flowRate * (minutes - (move + 1))));
             }
@@ -305,8 +310,14 @@ public class Day16 : ISolver
         return solutions.Max();
     }
 
+    private Dictionary<(string from, string to), int> Paths = new Dictionary<(string from, string to), int>();
     private int GetPath(string current, string destination, List<Valve> valves)
     {
+        if (Paths.ContainsKey((current, destination)))
+        {
+            return Paths[(current, destination)];
+        }
+
         var toCheck = new List<(string name, int distance)>();
         var hasChecked = new HashSet<string>();
         toCheck.Add((current, 0));
@@ -319,6 +330,7 @@ public class Day16 : ISolver
 
             if (node.Connections.Contains(destination))
             {
+                Paths.Add((current, destination), t.distance + 1);
                 return t.distance + 1;
             }
 
@@ -333,9 +345,16 @@ public class Day16 : ISolver
     }
 }
 
-public class Valve
+public record Valve
 {
     public string Name { get; set; }
     public int flowRate { get; set; }
     public List<string> Connections { get; set; }
 }
+//
+// public record Players
+// {
+//     public ()
+//
+//     public bool Equ
+// }
