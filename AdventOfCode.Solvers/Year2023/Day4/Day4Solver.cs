c7c515a2cdae7453cd2cf11d7251f21a04e26628
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-12-04 07:07:55 +0100

    improved code

--- AdventOfCode.Solvers/Year2023/Day4/Day4Solver.cs
+++ AdventOfCode.Solvers/Year2023/Day4/Day4Solver.cs
@@ -16,18 +16,19 @@ public class Day4Solver : BaseSolver
         {
             var cardrun = 1;
             bool first = true;
+
+            var nums = line.Split(':').Last();
+            var winningNumbers = nums.Split('|').First().Split(' ')
+            .Where(s => !string.IsNullOrWhiteSpace(s))
+            .Select(int.Parse).Order().ToList();
+            var numbersYouHave = nums.Split('|').Last().Split(' ')
+            .Where(s => !string.IsNullOrWhiteSpace(s))
+            .Select(int.Parse).Order().ToList();
+
+            var matches = winningNumbers.Intersect(numbersYouHave).ToList().Count();
+
             do
             {
-                var nums = line.Split(':').Last();
-                var winningNumbers = nums.Split('|').First().Split(' ')
-                .Where(s => !string.IsNullOrWhiteSpace(s))
-                .Select(int.Parse).Order().ToList();
-                var numbersYouHave = nums.Split('|').Last().Split(' ')
-                .Where(s => !string.IsNullOrWhiteSpace(s))
-                .Select(int.Parse).Order().ToList();
-
-                var matches = winningNumbers.Intersect(numbersYouHave).ToList().Count();
-
                 for(int i = 1; i <= matches; i++)
                 {
                     if (position + i < cards.Count)
