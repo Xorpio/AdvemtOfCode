cbf8ee1d056b0c081ebc000c44cd844cededcd1d
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-12-06 06:50:53 +0100

    added initial classes

---
+++ AdventOfCode.Solvers/Year2023/Day6/Day6Solver.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Solvers.Year2023.Day6;
+
+public class Day6Solver : BaseSolver
+{
+    public override void Solve(string[] puzzle)
+    {
+        throw new NotImplementedException();
+        GiveAnswer1("");
+        GiveAnswer2("");
+    }
+}
