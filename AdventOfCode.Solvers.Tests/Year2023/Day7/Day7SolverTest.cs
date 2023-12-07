06ae994d9069b11aa98a2bd8075db767dd9cc147
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-12-07 08:35:11 +0100

    part 1 day 7

--- AdventOfCode.Solvers.Tests/Year2023/Day7/Day7SolverTest.cs
+++ AdventOfCode.Solvers.Tests/Year2023/Day7/Day7SolverTest.cs
@@ -7,7 +7,11 @@ public class Day7SolverTest
     private readonly ITestOutputHelper _output;
 
     private string example = """
-    sampleinput
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
     """;
 
     public Day7SolverTest(ITestOutputHelper output)
@@ -28,7 +32,7 @@ public class Day7SolverTest
 
         var answer = await solver.Answer1.LastAsync();
 
-        answer.Should().Be("?");
+        answer.Should().Be("6440");
     }
 
     [Fact(DisplayName ="2023 Day 7 Solver Has Correct Solution For Part 2 sample input")]
@@ -45,4 +49,47 @@ public class Day7SolverTest
         var answer = await solver.Answer2.LastAsync();
 
         answer.Should().Be("?");
-    }}
+    }
+
+    [Theory]
+    [InlineData("33332 1", "2AAAA 1")]
+    [InlineData("77888 1", "77788 1")]
+    [InlineData("A1111 1", "K1111 1")]
+    [InlineData("K1111 1", "Q1111 1")]
+    [InlineData("Q1111 1", "J1111 1")]
+    [InlineData("J1111 1", "T1111 1")]
+    [InlineData("T1111 1", "91111 1")]
+    [InlineData("91111 1", "81111 1")]
+    [InlineData("81111 1", "71111 1")]
+    [InlineData("61111 1", "51111 1")]
+    [InlineData("51111 1", "41111 1")]
+    [InlineData("41111 1", "31111 1")]
+    [InlineData("31111 1", "21111 1")]
+    [InlineData("12346 1", "12345 1")]
+    public void CompareHands(string in1, string in2)
+    {
+        var hand1 = new Hand(in1);
+        var hand2 = new Hand(in2);
+
+        hand1.CompareTo(hand2).Should().BePositive();
+    }
+
+    [Fact]
+    public void equalThrows()
+    {
+        var hand1 = new Hand("12345 1");
+        var hand2 = new Hand("12345 2");
+
+        var act = () => hand1.CompareTo(hand2);
+
+        act.Should().Throw<Exception>();
+    }
+
+    [Fact]
+    public void ThreeOfAKind()
+    {
+        var input = "54J34 824";
+        var hand = new Hand(input);
+        hand.HandType.Should().Be(TypeOfHand.OnePair);
+    }
+}
--- AdventOfCode.Solvers/Year2023/Day7/Day7Solver.cs
+++ AdventOfCode.Solvers/Year2023/Day7/Day7Solver.cs
@@ -1,11 +1,131 @@
+using System.Reactive.Subjects;
+
 namespace AdventOfCode.Solvers.Year2023.Day7;
 
 public class Day7Solver : BaseSolver
 {
     public override void Solve(string[] puzzle)
     {
-        throw new NotImplementedException();
-        GiveAnswer1("");
+        var listOfHands = new List<Hand>();
+        foreach(var line in puzzle)
+        {
+            var hand = new Hand(line.ToUpper());
+            hand.Logger.Subscribe(logger);
+            listOfHands.Add(hand);
+        }
+
+        listOfHands.Sort();
+
+        double count = 1;
+        double part1 = 0;
+        foreach(var hand in listOfHands)
+        {
+            part1 += hand.Bid * count;
+            count++;
+        }
+
+        GiveAnswer1(part1);
         GiveAnswer2("");
     }
+}
+
+public class Hand : IComparable<Hand>
+{
+    public ReplaySubject<string> Logger { get; set; }  = new ReplaySubject<string>();
+    public TypeOfHand HandType { get; set; }
+    public string HandInput { get; set; }
+    public double Bid { get; set; }
+    public Hand(string input)
+    {
+        var splits = input.Split(' ');
+        HandType = determineHandType(splits[0]);
+        HandInput = splits[0];
+        Bid = double.Parse(splits[1]);
+    }
+
+    private TypeOfHand determineHandType(string input)
+    {
+        var groups = input.GroupBy(c => c);
+
+        var returnType = TypeOfHand.HighCard;
+
+        if (groups.Any(g => g.Count() == 5))
+        {
+            returnType = TypeOfHand.FiveOfAKind;
+        }
+        if (groups.Any(g => g.Count() == 4))
+        {
+            returnType = TypeOfHand.FourOfAKind;
+        }
+        if (groups.Any(g => g.Count() == 3) && groups.Any(g => g.Count() == 2))
+        {
+            returnType = TypeOfHand.FullHouse;
+        }
+        if (groups.Any(g => g.Count() == 3))
+        {
+            returnType = TypeOfHand.ThreeOfAKind;
+        }
+        if (groups.Count(g => g.Count() == 2) == 2)
+        {
+            returnType = TypeOfHand.TwoPair;
+        }
+        if (groups.Any(g => g.Count() == 2))
+        {
+            returnType = TypeOfHand.OnePair;
+        }
+        Logger.OnNext($"{input} is {returnType}");
+        return returnType;
+    }
+
+    public int CompareTo(Hand? other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        if (HandType != other.HandType)
+        {
+            return (int)HandType - (int)other.HandType;
+        }
+
+        foreach(var (card, otherCard) in HandInput.Zip(other.HandInput))
+        {
+            var cardValue = compareCard(card);
+            var otherCardValue = compareCard(otherCard);
+
+            if (cardValue != otherCardValue)
+            {
+                return cardValue - otherCardValue;
+            }
+        }
+
+        if(other.HandInput == HandInput && other.Bid == Bid)
+        {
+            return 0;
+        }
+
+        throw new Exception("Shouldn't get here");
+    }
+
+    private int compareCard(char card) => card switch
+    {
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => int.Parse(card.ToString())
+    };
+}
+
+public enum TypeOfHand
+{
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0,
 }
