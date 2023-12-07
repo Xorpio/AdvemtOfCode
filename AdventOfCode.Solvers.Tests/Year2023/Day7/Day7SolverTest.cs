8a1df541d9f2102c81c89a8bb2135a80f068aae7
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-12-07 07:14:43 +0100

    day 7 initial

---
+++ AdventOfCode.Solvers.Tests/Year2023/Day7/Day7SolverTest.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode.Solvers.Year2023.Day7;
+
+namespace AdventOfCode.Solvers.Tests.Year2023.Day7;
+
+public class Day7SolverTest
+{
+    private readonly ITestOutputHelper _output;
+
+    private string example = """
+    sampleinput
+    """;
+
+    public Day7SolverTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact(DisplayName ="2023 Day 7 Solver Has Correct Solution For Part 1 sample input")]
+    public async Task Day7SolverHasCorrectSolutionForPart1SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day7Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer1.LastAsync();
+
+        answer.Should().Be("?");
+    }
+
+    [Fact(DisplayName ="2023 Day 7 Solver Has Correct Solution For Part 2 sample input")]
+    public async Task Day7SolverHasCorrectSolutionForPart2SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day7Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer2.LastAsync();
+
+        answer.Should().Be("?");
+    }}
---
+++ AdventOfCode.Solvers/Year2023/Day7/Day7Solver.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Solvers.Year2023.Day7;
+
+public class Day7Solver : BaseSolver
+{
+    public override void Solve(string[] puzzle)
+    {
+        throw new NotImplementedException();
+        GiveAnswer1("");
+        GiveAnswer2("");
+    }
+}
