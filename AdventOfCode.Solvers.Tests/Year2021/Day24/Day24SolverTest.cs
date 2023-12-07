fa97c4e87353265eaad25ff7cc182a71c689c8ef
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-12-07 22:10:23 +0100

    part 1 and 2 done 🦪

--- AdventOfCode.Solvers.Tests/Year2021/Day24/Day24SolverTest.cs
+++ AdventOfCode.Solvers.Tests/Year2021/Day24/Day24SolverTest.cs
@@ -7,7 +7,23 @@ public class Day24SolverTest
     private readonly ITestOutputHelper _output;
 
     private string example = """
-    sampleinput
+    mul x 0
+    add x z
+    mod x 26
+    div z 1
+    add x 14
+    eql x w
+    eql x 0
+    mul y 0
+    add y 25
+    mul y x
+    add y 1
+    mul z y
+    mul y 0
+    add y w
+    add y 12
+    mul y x
+    add z y
     """;
 
     public Day24SolverTest(ITestOutputHelper output)
@@ -48,118 +64,131 @@ public class Day24SolverTest
     }
 
     [Fact]
-    public void basicAlu()
+    public void test()
     {
-        var alu = new ALU(Array.Empty<string>());
+        for(double i = -10; i < 30; i++)
+        {
+            var ans = Math.Floor(i / 26);
 
-        alu.W.Should().Be(0);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(0);
+            _output.WriteLine($"{i} / 26 = {ans}");
+            ans.Should().Be(0); 
+        }
+
     }
 
-    [Fact]
-    public void basicAluinpa()
-    {
-        var alu = new ALU(new[] { "inp w" });
+    // [Fact]
+    // public void basicAlu()
+    // {
+    //     var alu = new ALU(Array.Empty<string>());
 
-        alu.Run(new int[] {1});
+    //     alu.W.Should().Be(0);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(0);
+    // }
 
-        alu.W.Should().Be(1);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(0);
-    }
+    // [Fact]
+    // public void basicAluinpa()
+    // {
+    //     var alu = new ALU(new[] { "inp w" });
 
-    [Fact]
-    public void basicAluAdd()
-    {
-        var alu = new ALU(new[] { "inp w", "add z w" });
+    //     alu.Run(new int[] {1});
 
-        alu.Run(new int[] {1});
+    //     alu.W.Should().Be(1);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(0);
+    // }
 
-        alu.W.Should().Be(1);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(1);
-    }
+    // [Fact]
+    // public void basicAluAdd()
+    // {
+    //     var alu = new ALU(new[] { "inp w", "add z w" });
 
-    [Fact]
-    public void basicAluMul()
-    {
-        var alu = new ALU(new[] { "inp w", "mul w -1" });
+    //     alu.Run(new int[] {1});
 
-        alu.Run(new int[] {1});
+    //     alu.W.Should().Be(1);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(1);
+    // }
 
-        alu.W.Should().Be(-1);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(0);
-    }
+    // [Fact]
+    // public void basicAluMul()
+    // {
+    //     var alu = new ALU(new[] { "inp w", "mul w -1" });
 
-    [Fact]
-    public void basicAluDiv()
-    {
-        var alu = new ALU(new[] { "inp w", "div w 2" });
+    //     alu.Run(new int[] {1});
 
-        alu.Run(new int[] {5});
+    //     alu.W.Should().Be(-1);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(0);
+    // }
 
-        alu.W.Should().Be(2);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(0);
-    }
+    // [Fact]
+    // public void basicAluDiv()
+    // {
+    //     var alu = new ALU(new[] { "inp w", "div w 2" });
 
-    [Fact]
-    public void basicAlumod()
-    {
-        var alu = new ALU(new[] { "inp w", "mod w 3" });
+    //     alu.Run(new int[] {5});
 
-        alu.Run(new int[] {1});
+    //     alu.W.Should().Be(2);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(0);
+    // }
 
-        alu.W.Should().Be(1);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(0);
-    }
+    // [Fact]
+    // public void basicAlumod()
+    // {
+    //     var alu = new ALU(new[] { "inp w", "mod w 3" });
 
-    [Fact]
-    public void basicAluEql()
-    {
-        var alu = new ALU(new[] { "inp w", "eql w 10" });
+    //     alu.Run(new int[] {1});
 
-        alu.Run(new int[] {1});
+    //     alu.W.Should().Be(1);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(0);
+    // }
 
-        alu.W.Should().Be(0);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.Z.Should().Be(0);
-    }
+    // [Fact]
+    // public void basicAluEql()
+    // {
+    //     var alu = new ALU(new[] { "inp w", "eql w 10" });
 
-    [Fact]
-    public void testmod()
-    {
-        var puzzle = """
-                     inp w
-                     add z w
-                     mod z 2
-                     div w 2
-                     add y w
-                     mod y 2
-                     div w 2
-                     add x w
-                     mod x 2
-                     div w 2
-                     mod w 2
-                     """;
+    //     alu.Run(new int[] {1});
 
-        var alu = new ALU(puzzle.Split(Environment.NewLine));
+    //     alu.W.Should().Be(0);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.Z.Should().Be(0);
+    // }
 
-        alu.Run(new int[] {9});
+    // [Fact]
+    // public void testmod()
+    // {
+    //     var puzzle = """
+    //                  inp w
+    //                  add z w
+    //                  mod z 2
+    //                  div w 2
+    //                  add y w
+    //                  mod y 2
+    //                  div w 2
+    //                  add x w
+    //                  mod x 2
+    //                  div w 2
+    //                  mod w 2
+    //                  """;
 
-        alu.Z.Should().Be(1);
-        alu.Y.Should().Be(0);
-        alu.X.Should().Be(0);
-        alu.W.Should().Be(1);
-    }
+    //     var alu = new ALU(puzzle.Split(Environment.NewLine));
+
+    //     alu.Run(new int[] {9});
+
+    //     alu.Z.Should().Be(1);
+    //     alu.Y.Should().Be(0);
+    //     alu.X.Should().Be(0);
+    //     alu.W.Should().Be(1);
+    // }
 }
--- AdventOfCode.Solvers/Year2021/Day24/Day24Solver.cs
+++ AdventOfCode.Solvers/Year2021/Day24/Day24Solver.cs
@@ -1,6 +1,3 @@
-using System.Diagnostics;
-using System.Net.NetworkInformation;
-
 namespace AdventOfCode.Solvers.Year2021.Day24;
 
 public class Day24Solver : BaseSolver
@@ -10,48 +7,51 @@ public class Day24Solver : BaseSolver
     {
         _puzzle = puzzle;
 
-        var part1 = solvePuzzle(0, 0, 0) ?? throw new Exception();
+        var alu = new List<(bool, Func<int, int, int>)>();
+        var countInBlock = 1;
+        var aluInstruction = new Func<int, int, int>((w, z) => 0);
+        bool isType1 = false;
+        for (int i = 1; i < _puzzle.Length; i++)
+        {
+            if (_puzzle[i] == "inp w")
+            {
+                countInBlock = 0;
+                alu.Add((isType1, aluInstruction));
+            }
+        
+            if (countInBlock == 4)
+            {
+                isType1 = _puzzle[i] == "div z 1";
+            }
+        
+            if (countInBlock == 5 && !isType1)
+            {
+                var v = int.Parse(_puzzle[i].Split(' ')[2]);
+                aluInstruction = (w, z) =>
+                    {
+                        var result =  (z % 26) + v;
+                        return result;
+                    };
+            }
 
-        // var alu = new List<(bool, Func<int, int, int>)>();
-        // var countInBlock = 1;
-        // var aluInstruction = new Func<int, int, int>((w, z) => 0);
-        // bool isType1 = false;
-        // for (int i = 1; i < _puzzle.Length; i++)
-        // {
-        //     if (_puzzle[i] == "inp w")
-        //     {
-        //         countInBlock = 0;
-        //         alu.Add((isType1, aluInstruction));
-        //     }
-        //
-        //     if (countInBlock == 4)
-        //     {
-        //         isType1 = _puzzle[i] == "div z 1";
-        //     }
-        //
-        //     if (countInBlock == 5)
-        //     {
-        //         logger.OnNext(_puzzle[i]);
-        //         var v = int.Parse(_puzzle[i].Split(' ')[2]);
-        //         aluInstruction = isType1 ?
-        //             (w, z) => (26 * z) + w + v:
-        //             (w, z) =>
-        //             {
-        //                 var result =  (z % 26) + v;
-        //                 // logger.OnNext($"(z % 26) + v -=- ({z} % 26) + {v} = {result}");
-        //                 return result;
-        //             };
-        //     }
-        //
-        //     countInBlock++;
-        // }
-        //
-        // alu.Add((isType1, aluInstruction));
-        //
-        // var a = s(alu.ToArray(), 0);
+            if (countInBlock == 15 && isType1)
+            {
+                var v = int.Parse(_puzzle[i].Split(' ')[2]);
+                logger.OnNext(_puzzle[i]);
+                aluInstruction = (w, z) => (26 * z) + w + v;
+            }
+        
+            countInBlock++;
+        }
+        
+        alu.Add((isType1, aluInstruction));
+        
+        var a = s(alu.ToArray(), 0);
 
-        GiveAnswer1(part1);
-        GiveAnswer2("");
+        var b = s2(alu.ToArray(), 0);
+
+        GiveAnswer1(a);
+        GiveAnswer2(b);
     }
 
     private string s((bool type, Func<int,int,int> f)[] alu, int z)
@@ -72,7 +72,7 @@ public class Day24Solver : BaseSolver
 
                 if (result != null)
                 {
-                    return $"{w}-{result}";
+                    return $"{w}{result}";
                 }
             }
 
@@ -83,225 +83,50 @@ public class Day24Solver : BaseSolver
         var newZ = (int)Math.Floor((decimal)z / 26);
         if (alu.Length == 1)
         {
-            return newZ == 0 ? $"{newW}" : null;
+            return newZ == 0 && newW > 0 && newW <= 9 ? $"{newW}" : null;
         }
 
         if (newW > 9 || newW == 0)
             newZ *= 26;
         var res = s(alu[1..], newZ);
-        return (res == null) ? null : $"{newW}+{res}";
+        return (res == null) ? null : $"{newW}{res}";
     }
 
-    public string? solvePuzzle(int count, int z, int level, string prefix = "")
+    private string s2((bool type, Func<int,int,int> f)[] alu, int z)
     {
-        var instruction = _puzzle[count];
-
-        //check for z / 26
-        var divInstruction = getZDivInstruction(count);
-
-        if (divInstruction.EndsWith("1"))
+        var (type, f) = alu[0];
+        if (type)
         {
-            for (int n = 9; n > 0; n--)
+            for (int w = 1; w <= 10; w++)
             {
-                var (newZ, newCount) = runBlock(count + 1, n, z);
+                var newz = f(w, z);
 
-                if (newCount == _puzzle.Length)
+                if (alu.Length == 1)
                 {
-                    return newZ == 0 ? $"{n}" : null;
+                    return newz == 0 ? $"{w}" : null;
                 }
 
-                var result = solvePuzzle(newCount, newZ, level + 1, $"{prefix}{n}");
+                var result = s2(alu[1..], newz);
+
                 if (result != null)
                 {
-                    return $"{n}{result}";
-                }
-            }
-        }
-        else
-        {
-            //calc n
-            for (int n = 9; n > 0; n--)
-            {
-                var (newZ, newCount) = runBlock(count + 1, n, z);
-
-                if (newZ < z)
-                {
-                    if (newCount == _puzzle.Length)
-                    {
-                        return newZ == 0 ? $"{n}" : null;
-                    }
-
-                    var result = solvePuzzle(newCount, newZ, level + 1, $"{prefix}{n}");
-                    if (result != null)
-                    {
-                        return $"{n}{result}";
-                    }
+                    return $"{w}{result}";
                 }
             }
 
             return null;
         }
 
-        return null;
-    }
-
-    public (int z, int count) runBlock(int count, int w, int z)
-    {
-        var x = 0;
-        var y = 0;
-        var line = _puzzle[count];
-        do
+        var newW = f(0, z);
+        var newZ = (int)Math.Floor((decimal)z / 26);
+        if (alu.Length == 1)
         {
-            var instructions = line.Split(' ');
-
-            //get value
-            var value = instructions[2] switch
-            {
-                "w" => w,
-                "x" => x,
-                "y" => y,
-                "z" => z,
-                _ => int.Parse(instructions[2])
-            };
-
-            switch (instructions[0])
-            {
-                case "mul":
-                    switch (instructions[1])
-                    {
-                        case "w":
-                            w = w * value;
-                            break;
-                        case "x":
-                            x = x * value;
-                            break;
-                        case "y":
-                            y = y * value;
-                            break;
-                        case "z":
-                            z = z * value;
-                            break;
-                        default: throw new Exception();
-                    }
-                    break;
-                case "add":
-                    switch (instructions[1])
-                    {
-                        case "w":
-                            w = w + value;
-                            break;
-                        case "x":
-                            x = x +value;
-                            break;
-                        case "y":
-                            y = y + value;
-                            break;
-                        case "z":
-                            z = z + value;
-                            break;
-                        default: throw new Exception();
-                    }
-                    break;
-                case "div":
-                    if (value != 0)
-                    {
-                        switch (instructions[1])
-                        {
-                            case "w":
-                                w = (int)Math.Floor((decimal)(w / value));
-                                break;
-                            case "x":
-                                x = (int)Math.Floor((decimal)(x / value));
-                                break;
-                            case "y":
-                                y = (int)Math.Floor((decimal)(y / value));
-                                break;
-                            case "z":
-                                z = (int)Math.Floor((decimal)(z / value));
-                                break;
-                            default: throw new Exception();
-                        }
-                    }
-                    break;
-                case "mod":
-                    if (value > 0)
-                    {
-                        switch (instructions[1])
-                        {
-                            case "w":
-                                if (w >= 0)
-                                {
-                                    w = w % value;
-                                }
-                                break;
-                            case "x":
-                                if (x >= 0)
-                                {
-                                    x = x % value;
-                                }
-                                break;
-                            case "y":
-                                if (y >= 0)
-                                {
-                                    y = y % value;
-                                }
-                                break;
-                            case "z":
-                                if (z >= 0)
-                                {
-                                    z = z % value;
-                                }
-                                break;
-                            default: throw new Exception();
-                        }
-                    }
-                    break;
-                case "eql":
-                    switch (instructions[1])
-                    {
-                        case "w":
-                            w = w == value ? 1 : 0;
-                            break;
-                        case "x":
-                            x = x == value ? 1 : 0;
-                            break;
-                        case "y":
-                            y = y == value ? 1 : 0;
-                            break;
-                        case "z":
-                            z = z == value ? 1 : 0;
-                            break;
-                        default: throw new Exception();
-                    }
-                    break;
-                default: throw new Exception();
-            }
-
-            count++;
-
-            if (count == _puzzle.Length)
-            {
-                return (z, count);
-            }
-
-            line = _puzzle[count];
-        } while (!line.StartsWith("inp"));
-
-        return (z, count);
-    }
-
-    private string getZDivInstruction(int count)
-    {
-        for(int i = count; i < _puzzle.Length; i++)
-        {
-            var instruction = _puzzle[i];
-
-            if(instruction.StartsWith("div z"))
-            {
-                return instruction;
-            }
+            return newZ == 0 && newW > 0 && newW <= 9 ? $"{newW}" : null;
         }
 
-        throw new Exception("no div instruction found");
+        if (newW > 9 || newW < 1)
+            newZ *= 26;
+        var res = s2(alu[1..], newZ);
+        return (res == null) ? null : $"{newW}{res}";
     }
 }
