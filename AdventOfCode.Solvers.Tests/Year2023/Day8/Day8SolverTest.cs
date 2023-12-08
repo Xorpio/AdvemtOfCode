0c2ab4d5a854716a141176549d558b44511b2eae
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2023-12-08 09:39:14 +0100

    part 1 done

--- AdventOfCode.Solvers.Tests/Year2023/Day8/Day8SolverTest.cs
+++ AdventOfCode.Solvers.Tests/Year2023/Day8/Day8SolverTest.cs
@@ -7,7 +7,15 @@ public class Day8SolverTest
     private readonly ITestOutputHelper _output;
 
     private string example = """
-    sampleinput
+    RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)
     """;
 
     public Day8SolverTest(ITestOutputHelper output)
@@ -28,13 +36,25 @@ public class Day8SolverTest
 
         var answer = await solver.Answer1.LastAsync();
 
-        answer.Should().Be("?");
+        answer.Should().Be("2");
     }
 
     [Fact(DisplayName ="2023 Day 8 Solver Has Correct Solution For Part 2 sample input")]
     public async Task Day8SolverHasCorrectSolutionForPart2SampleInputAsync()
     {
-        var lines = example.Split(Environment.NewLine);
+        var e = """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+        """;
+        var lines = e.Split(Environment.NewLine);
 
         var solver = new Day8Solver();
 
@@ -44,5 +64,5 @@ public class Day8SolverTest
 
         var answer = await solver.Answer2.LastAsync();
 
-        answer.Should().Be("?");
+        answer.Should().Be("6");
     }}
--- AdventOfCode.Solvers/Year2023/Day8/Day8Solver.cs
+++ AdventOfCode.Solvers/Year2023/Day8/Day8Solver.cs
@@ -1,11 +1,74 @@
+using System.Dynamic;
+
 namespace AdventOfCode.Solvers.Year2023.Day8;
 
 public class Day8Solver : BaseSolver
 {
     public override void Solve(string[] puzzle)
     {
-        throw new NotImplementedException();
-        GiveAnswer1("");
-        GiveAnswer2("");
+        var map = new Dictionary<string, (string left, string right)>();
+
+        string key = "";
+        foreach (var line in puzzle.Skip(2))
+        {
+            var parts = line.Split(" = ");
+            key = parts[0];
+            parts = parts[1].Split(", ");
+            var left = parts[0][1..].Trim();
+            var right = parts[1][..^1].Trim();
+
+            map.Add(key, (left, right));
+        }
+
+        foreach(var m in map)
+        {
+            logger.OnNext($"{m.Key} = {m.Value}");
+        }
+
+        key = "AAA";
+        var step = 0;
+        var instruction = puzzle[0];
+        // do
+        // {
+        //     var i = instruction[step % instruction.Length];
+        //     key = i switch
+        //     {
+        //         'R' => map[key].right,
+        //         'L' => map[key].left,
+        //         _ => throw new Exception("Unknown instruction")
+        //     };
+
+        //     step++;
+        // } while(key != "ZZZ");
+
+        // GiveAnswer1(step);
+
+        var keys = map.Select(k => k.Key).Where(k => k.EndsWith("A")).ToList();
+        foreach(var k in keys)
+        {
+            logger.OnNext($"{k} = {map[k]}");
+        }
+        step = 0;
+        do
+        {
+            var i = instruction[step % instruction.Length];
+            var temp = new List<string>();
+
+            foreach(var k in keys)
+            {
+                temp.Add(i switch
+                {
+                    'R' => map[k].right,
+                    'L' => map[k].left,
+                    _ => throw new Exception("Unknown instruction")
+                });
+            }
+
+            keys = temp;
+            step++;
+        } while(!keys.All(k =>k.EndsWith("Z")));        
+
+
+        GiveAnswer2(step);
     }
 }
