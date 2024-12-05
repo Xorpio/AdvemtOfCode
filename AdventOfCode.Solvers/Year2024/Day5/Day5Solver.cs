4657ff9c2f95cdf6eeab4066ab0f15c64a8149c1
Gooijer <3060868+Xorpio@users.noreply.github.com>
2024-12-05 21:39:53 +0100

    solved part 1 day 5

--- AdventOfCode.Solvers/Year2024/Day5/Day5Solver.cs
+++ AdventOfCode.Solvers/Year2024/Day5/Day5Solver.cs
@@ -1,11 +1,59 @@
+using System.Security;
+
 namespace AdventOfCode.Solvers.Year2024.Day5;
 
 public class Day5Solver : BaseSolver
 {
     public override void Solve(string[] puzzle)
     {
-        throw new NotImplementedException();
-        GiveAnswer1("");
+        var lookup = new Dictionary<string, IList<string>>();
+
+        var answer1 = 0;
+
+        bool parsing = true;
+        foreach(var line in puzzle)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                parsing = false;
+                continue;
+            }
+
+            if (parsing)
+            {
+                var parts = line.Split('|');
+                if (!lookup.ContainsKey(parts[0]))
+                {
+                    lookup.Add(parts[0], []);
+                }
+
+                lookup[parts[0]].Add(parts[1]);
+            }
+            else
+            {
+                var pages = line.Split(',');
+                if (validateLine(pages, lookup))
+                {
+                    answer1 += int.Parse(pages[(int)decimal.Floor(pages.Length / 2)]);
+                }
+            }
+        }
+
+        GiveAnswer1(answer1);
         GiveAnswer2("");
+    }
+
+    public bool validateLine(string[] lineParts, Dictionary<string, IList<string>> lookup)
+    {
+        var hist = new HashSet<string>();
+        foreach(var part in lineParts)
+        {
+            if (lookup.ContainsKey(part) && lookup[part].Any(hist.Contains))
+            {
+                return false;
+            }
+            hist.Add(part);
+        }
+        return true;
     }
 }
