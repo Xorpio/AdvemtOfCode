7bb93462311ad82a7135d4c43472f188b5730fb2
Xorpio <3060868+Xorpio@users.noreply.github.com>
2024-12-06 06:44:22 +0100

    feat: new day 6

---
+++ AdventOfCode.Solvers.Tests/Year2024/Day6/Day6SolverTest.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode.Solvers.Year2024.Day6;
+
+namespace AdventOfCode.Solvers.Tests.Year2024.Day6;
+
+public class Day6SolverTest
+{
+    private readonly ITestOutputHelper _output;
+
+    private string example = """
+    sampleinput
+    """;
+
+    public Day6SolverTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact(DisplayName = "2024 Day 6 Solver Has Correct Solution For Part 1 sample input")]
+    public async Task Day6SolverHasCorrectSolutionForPart1SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day6Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer1.LastAsync();
+
+        answer.Should().Be("?");
+    }
+
+    [Fact(DisplayName = "2024 Day 6 Solver Has Correct Solution For Part 2 sample input")]
+    public async Task Day6SolverHasCorrectSolutionForPart2SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day6Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer2.LastAsync();
+
+        answer.Should().Be("?");
+    }
+}
