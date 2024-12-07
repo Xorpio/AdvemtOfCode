82b29926116e766a9e50311c5ab092e9a6dc2101
Gooijer <3060868+Xorpio@users.noreply.github.com>
2024-12-07 16:48:31 +0100

    day 7 part 2 start

--- AdventOfCode.Solvers/Year2024/Day7/Day7Solver.cs
+++ AdventOfCode.Solvers/Year2024/Day7/Day7Solver.cs
@@ -5,6 +5,7 @@ public class Day7Solver : BaseSolver
     public override void Solve(string[] puzzle)
     {
         decimal total = 0;
+        decimal total2 = 0;
 
         foreach(var line in puzzle)
         {
@@ -17,13 +18,19 @@ public class Day7Solver : BaseSolver
             {
                 total += expected;
             }
+
+            isValid = recCalc(expected, inputs, [], true);
+            if (isValid)
+            {
+                total2 += expected;
+            }
         }
 
         GiveAnswer1(total);
-        GiveAnswer2("");
+        GiveAnswer2(total2);
     }
 
-    private bool recCalc(decimal expected, decimal[] inputs, Opp[] opps)
+    private bool recCalc(decimal expected, decimal[] inputs, Opp[] opps, bool isPart2 = false)
     {
         //validate
         if ((opps.Length + 1) == inputs.Length)
@@ -36,7 +43,8 @@ public class Day7Solver : BaseSolver
             {
                 total = o switch {
                     Opp.mult => total * inputs[c],
-                    _ => total + inputs[c],
+                    Opp.comb => total + inputs[c],
+                    _ => throw new NotImplementedException()
                 };
 
                 c++;
@@ -58,6 +66,14 @@ public class Day7Solver : BaseSolver
             return true;
         }
 
+        if (!isPart2)
+        {
+            if (recCalc(expected, inputs, opps.Append(Opp.comb).ToArray()))
+            {
+                return true;
+            }
+        }
+
         return false;
     }
 }
@@ -65,5 +81,6 @@ public class Day7Solver : BaseSolver
 enum Opp
 {
     plus,
-    mult
+    mult,
+    comb
 }
