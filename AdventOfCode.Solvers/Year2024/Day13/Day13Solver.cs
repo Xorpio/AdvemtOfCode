2eba3c3a07898a09af3aab44f6723253715f85b4
Xorpio <3060868+Xorpio@users.noreply.github.com>
2024-12-13 06:47:30 +0100

    feat: boilerplate day 14

---
+++ AdventOfCode.Solvers/Year2024/Day13/Day13Solver.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Solvers.Year2024.Day13;
+
+public class Day13Solver : BaseSolver
+{
+    public override void Solve(string[] puzzle)
+    {
+        throw new NotImplementedException();
+        GiveAnswer1("");
+        GiveAnswer2("");
+    }
+}
