e77ec33fa3bfcfbfc24551c5f9a3dfca7aed58b9
Gooijer <3060868+Xorpio@users.noreply.github.com>
2024-12-14 09:16:36 +0100

    day 14 boilerpllate code

---
+++ AdventOfCode.Solvers/Year2024/Day14/Day14Solver.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Solvers.Year2024.Day14;
+
+public class Day14Solver : BaseSolver
+{
+    public override void Solve(string[] puzzle)
+    {
+        throw new NotImplementedException();
+        GiveAnswer1("");
+        GiveAnswer2("");
+    }
+}
