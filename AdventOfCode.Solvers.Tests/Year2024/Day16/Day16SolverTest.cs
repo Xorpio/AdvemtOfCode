b0ee6aeeeb8a8ef970dbf5af1e17a299bfb2cbe7
Gooijer <3060868+Xorpio@users.noreply.github.com>
2024-12-16 07:42:56 +0100

    day 16 boilerplate code

---
+++ AdventOfCode.Solvers.Tests/Year2024/Day16/Day16SolverTest.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode.Solvers.Year2024.Day16;
+
+namespace AdventOfCode.Solvers.Tests.Year2024.Day16;
+
+public class Day16SolverTest
+{
+    private readonly ITestOutputHelper _output;
+
+    private string example = """
+    sampleinput
+    """;
+
+    public Day16SolverTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact(DisplayName ="2024 Day 16 Solver Has Correct Solution For Part 1 sample input")]
+    public async Task Day16SolverHasCorrectSolutionForPart1SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day16Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer1.LastAsync();
+
+        answer.Should().Be("?");
+    }
+
+    [Fact(DisplayName ="2024 Day 16 Solver Has Correct Solution For Part 2 sample input")]
+    public async Task Day16SolverHasCorrectSolutionForPart2SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day16Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer2.LastAsync();
+
+        answer.Should().Be("?");
+    }}
