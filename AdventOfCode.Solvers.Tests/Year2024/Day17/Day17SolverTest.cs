bfa0628fa5a5a5d1c085b954f5bb43115cd881bf
Xorpio <3060868+Xorpio@users.noreply.github.com>
2024-12-18 15:22:30 +0100

    day 17 boilerplate

---
+++ AdventOfCode.Solvers.Tests/Year2024/Day17/Day17SolverTest.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode.Solvers.Year2024.Day17;
+
+namespace AdventOfCode.Solvers.Tests.Year2024.Day17;
+
+public class Day17SolverTest
+{
+    private readonly ITestOutputHelper _output;
+
+    private string example = """
+    Register A: 729
+    Register B: 0
+    Register C: 0
+
+    Program: 0,1,5,4,3,0
+    """;
+
+    public Day17SolverTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact(DisplayName = "2024 Day 17 Solver Has Correct Solution For Part 1 sample input")]
+    public async Task Day17SolverHasCorrectSolutionForPart1SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day17Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer1.LastAsync();
+
+        answer.Should().Be("4,6,3,5,6,3,5,2,1,0");
+    }
+
+    [Fact(DisplayName = "2024 Day 17 Solver Has Correct Solution For Part 2 sample input")]
+    public async Task Day17SolverHasCorrectSolutionForPart2SampleInputAsync()
+    {
+        var lines = example.Split(Environment.NewLine);
+
+        var solver = new Day17Solver();
+
+        solver.Logger.Subscribe(msg => _output.WriteLine(msg));
+
+        solver.Solve(lines);
+
+        var answer = await solver.Answer2.LastAsync();
+
+        answer.Should().Be("?");
+    }
+}
---
+++ AdventOfCode.Solvers/Year2024/Day17/Day17Solver.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Solvers.Year2024.Day17;
+
+public class Day17Solver : BaseSolver
+{
+    public override void Solve(string[] puzzle)
+    {
+        throw new NotImplementedException();
+        GiveAnswer1("");
+        GiveAnswer2("");
+    }
+}
