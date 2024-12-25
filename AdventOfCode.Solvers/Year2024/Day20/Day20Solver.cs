b8ed68e42741c739187de50649917b752b4ad90b
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2024-12-25 18:18:24 +0100

    day 20 boilerplate

---
+++ AdventOfCode.Solvers/Year2024/Day20/Day20Solver.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Solvers.Year2024.Day20;
+
+public class Day20Solver : BaseSolver
+{
+    public override void Solve(string[] puzzle)
+    {
+        throw new NotImplementedException();
+        GiveAnswer1("");
+        GiveAnswer2("");
+    }
+}
