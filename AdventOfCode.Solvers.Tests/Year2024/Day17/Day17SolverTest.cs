a927142b85fcf16f63fea9bca10f79ae7996c506
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2024-12-25 09:05:07 +0100

    part 2 done

--- AdventOfCode.Solvers.Tests/Year2024/Day17/Day17SolverTest.cs
+++ AdventOfCode.Solvers.Tests/Year2024/Day17/Day17SolverTest.cs
@@ -6,7 +6,7 @@ public class Day17SolverTest
 {
     private readonly ITestOutputHelper _output;
 
-    private string example = """
+    private string example1 = """
     Register A: 729
     Register B: 0
     Register C: 0
@@ -22,7 +22,7 @@ public class Day17SolverTest
     [Fact(DisplayName = "2024 Day 17 Solver Has Correct Solution For Part 1 sample input")]
     public async Task Day17SolverHasCorrectSolutionForPart1SampleInputAsync()
     {
-        var lines = example.Split(Environment.NewLine);
+        var lines = example1.Split(Environment.NewLine);
 
         var solver = new Day17Solver();
 
@@ -38,7 +38,15 @@ public class Day17SolverTest
     [Fact(DisplayName = "2024 Day 17 Solver Has Correct Solution For Part 2 sample input")]
     public async Task Day17SolverHasCorrectSolutionForPart2SampleInputAsync()
     {
-        var lines = example.Split(Environment.NewLine);
+        string example2 = """
+        Register A: 2024
+        Register B: 0
+        Register C: 0
+
+        Program: 0,3,5,4,3,0
+        """;
+
+        var lines = example2.Split(Environment.NewLine);
 
         var solver = new Day17Solver();
 
@@ -48,6 +56,108 @@ public class Day17SolverTest
 
         var answer = await solver.Answer2.LastAsync();
 
-        answer.Should().Be("?");
+        answer.Should().Be("117440");
     }
+
+    [Fact(DisplayName = "Test sample 1")]
+    public void TestSample1()
+    {
+        // If register C contains 9, the program 2,6 would set register B to 1.
+        var lines = new string[]
+        {
+            "Register A: 0",
+            "Register B: 0",
+            "Register C: 9",
+            "",
+            "Program: 2,6"
+        };
+        System.Reactive.Subjects.ReplaySubject<string> logger = new System.Reactive.Subjects.ReplaySubject<string>();
+        logger.Subscribe(_output.WriteLine);
+        var device = new Device(lines, logger);
+
+        device.Run();
+        device.B.Should().Be(1);
+    }
+
+    [Fact(DisplayName = "Test sample 2")]
+    public void TestSample2()
+    {
+        // If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+        var lines = new string[]
+        {
+            "Register A: 10",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 5,0,5,1,5,4"
+        };
+        System.Reactive.Subjects.ReplaySubject<string> logger = new System.Reactive.Subjects.ReplaySubject<string>();
+        logger.Subscribe(_output.WriteLine);
+        var device = new Device(lines, logger);
+        var output = device.Run();
+
+        output.Should().Be("0,1,2");
+    }
+
+    [Fact(DisplayName = "Test sample 3")]
+    public void TestSample3()
+    {
+        // If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+
+        var lines = new string[]
+        {
+            "Register A: 2024",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 0,1,5,4,3,0"
+        };
+        System.Reactive.Subjects.ReplaySubject<string> logger = new System.Reactive.Subjects.ReplaySubject<string>();
+        logger.Subscribe(_output.WriteLine);
+        var device = new Device(lines, logger);
+        var output = device.Run();
+        output.Should().Be("4,2,5,6,7,7,7,7,3,1,0");
+        device.A.Should().Be(0);
+    }
+
+    [Fact(DisplayName = "Test sample 4")]
+    public void TestSample4()
+    {
+        // If register B contains 29, the program 1,7 would set register B to 26.
+
+        var lines = new string[]
+        {
+            "Register A: 0",
+            "Register B: 29",
+            "Register C: 0",
+            "",
+            "Program: 1,7"
+        };
+        System.Reactive.Subjects.ReplaySubject<string> logger = new System.Reactive.Subjects.ReplaySubject<string>();
+        logger.Subscribe(_output.WriteLine);
+        var device = new Device(lines, logger);
+        device.Run();
+        device.B.Should().Be(26);
+    }
+
+    [Fact(DisplayName = "Test sample 5")]
+    public void TestSample5()
+    {
+        // If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+
+        var lines = new string[]
+        {
+            "Register A: 0",
+            "Register B: 2024",
+            "Register C: 43690",
+            "",
+            "Program: 4,0"
+        };
+        System.Reactive.Subjects.ReplaySubject<string> logger = new System.Reactive.Subjects.ReplaySubject<string>();
+        logger.Subscribe(_output.WriteLine);
+        var device = new Device(lines, logger);
+        device.Run();
+        device.B.Should().Be(44354);
+    }
+
 }
--- AdventOfCode.Solvers/Year2024/Day17/Day17Solver.cs
+++ AdventOfCode.Solvers/Year2024/Day17/Day17Solver.cs
@@ -6,26 +6,140 @@ public class Day17Solver : BaseSolver
 {
     public override void Solve(string[] puzzle)
     {
-        var device = new Device(puzzle, logger);
+        var device = new Device(puzzle, logger, int.MaxValue);
         GiveAnswer1(device.Run());
-        GiveAnswer2("");
+
+        var inputs = puzzle[4].Split(": ")[1].Split(",").Select(decimal.Parse).ToArray();
+        var max = inputs.Length;
+
+        decimal count = 0;
+
+        if (puzzle[4] == "Program: 0,3,5,4,3,0")
+        {
+            count = findCountTest(inputs, 0);
+        }
+        if (puzzle[4] == "Program: 2,4,1,3,7,5,4,2,0,3,1,5,5,5,3,0")
+        {
+            count = findCountTestActual(inputs, 0);
+        }
+
+        puzzle[0] = $"Register A: {count}";
+        device = new Device(puzzle, new ReplaySubject<string>(), max);
+        var output = device.Run();
+        logger.OnNext($"A: {count} = output: {output}");
+
+        GiveAnswer2(count);
+    }
+
+    private decimal findCountTest(decimal[] input, decimal current)
+    {
+        var count = 0;
+        while (true)
+        {
+            decimal a = current * 8;
+            a = a + count;
+
+            if (Math.Floor(a / 8) % 8 == input[^1])
+            {
+                if (input.Length == 1)
+                {
+                    return a;
+                }
+                var answer = findCountTest(input[..^1], a);
+                if (answer != 0)
+                {
+                    return answer;
+                }
+            }
+            count++;
+
+            if (Math.Floor(a / 8) != current)
+                return 0;
+        }
+    }
+
+    private decimal findCountTestActual(decimal[] input, decimal current)
+    {
+        var count = 0;
+        while (true)
+        {
+            decimal a = current * 8;
+            a = a + count;
+
+            //2,4 : B = A % 8
+            decimal b = a % 8;
+            //1,3 : B = B xor 3
+            b = XorDecimals(b, 3);
+            //7,5 : C = Floor(A / (2 ^ B) )
+            decimal c = Math.Floor(a / (decimal)Math.Pow(2, (double)b));
+            //4,2 : B = B xor C
+            b = XorDecimals(b, c);
+            ////0,3 : A = Floor(A / (2 ^ 3) )
+            //a = Math.Floor(a / (decimal)Math.Pow(2, 3));
+            //1,5 : B = B xor 5
+            b = XorDecimals(b, 5);
+
+
+            if (b % 8 == input[^1])
+            {
+                if (input.Length == 1)
+                {
+                    return a;
+                }
+
+                logger.OnNext($"current: {current}, count: {count}, input: {string.Join(",", input[..^1])}, a: {a}");
+                var answer = findCountTestActual(input[..^1], a);
+                if (answer != 0)
+                {
+                    return answer;
+                }
+            }
+            count++;
+
+            if (Math.Floor(a / 8) != current)
+                return 0;
+        }
+    }
+    public static decimal XorDecimals(decimal a, decimal b)
+    {
+        // Convert decimals to byte arrays
+        byte[] bytesA = decimal.GetBits(a).SelectMany(BitConverter.GetBytes).ToArray();
+        byte[] bytesB = decimal.GetBits(b).SelectMany(BitConverter.GetBytes).ToArray();
+
+        // Perform XOR operation on byte arrays
+        byte[] resultBytes = new byte[bytesA.Length];
+        for (int i = 0; i < bytesA.Length; i++)
+        {
+            resultBytes[i] = (byte)(bytesA[i] ^ bytesB[i]);
+        }
+
+        // Convert result byte array back to decimal
+        int[] resultBits = new int[resultBytes.Length / 4];
+        for (int i = 0; i < resultBits.Length; i++)
+        {
+            resultBits[i] = BitConverter.ToInt32(resultBytes, i * 4);
+        }
+
+        return new decimal(resultBits);
     }
 }
 
 public class Device
 {
-    double A = 0;
-    double B = 0;
-    double C = 0;
+    public double A { get; set; } = 0;
+    public double B { get; set; } = 0;
+    public double C { get; set; } = 0;
 
     int pointer = 0;
+
+    decimal _max = 0;
 
     int[] program = [];
 
     IList<double> output = new List<double>();
     private readonly ReplaySubject<string> _logger;
 
-    public Device(string[] puzzle, System.Reactive.Subjects.ReplaySubject<string> logger)
+    public Device(string[] puzzle, System.Reactive.Subjects.ReplaySubject<string> logger, decimal max = int.MaxValue)
     {
         A = double.Parse(puzzle[0].Split(": ")[1]);
         B = double.Parse(puzzle[1].Split(": ")[1]);
@@ -33,6 +147,8 @@ public class Device
 
         program = puzzle[4].Split(": ")[1].Split(",").Select(int.Parse).ToArray();
         this._logger = logger;
+
+        _max = max;
     }
 
     public string Run()
@@ -47,18 +163,28 @@ public class Device
                 break;
             }
 
+            if (step > 1000 || output.Count > _max)
+            {
+                _logger.OnNext("might have infinte loop?");
+                break;
+            }
+
             switch (program[pointer])
             {
                 case 0:
+                    _logger.OnNext($"adv({program[pointer]}, {program[pointer + 1]})");
                     adv(program[pointer], program[pointer + 1]);
                     break;
                 case 1:
+                    _logger.OnNext($"bxl({program[pointer]}, {program[pointer + 1]})");
                     bxl(program[pointer], program[pointer + 1]);
                     break;
                 case 2:
+                    _logger.OnNext($"bst({program[pointer]}, {program[pointer + 1]})");
                     bst(program[pointer], program[pointer + 1]);
                     break;
                 case 3:
+                    _logger.OnNext($"jnz({program[pointer]}, {program[pointer + 1]})");
                     var p = pointer;
                     jnz(program[pointer], program[pointer + 1]);
                     if (p != pointer)
@@ -67,15 +193,29 @@ public class Device
                     }
                     break;
                 case 4:
+                    _logger.OnNext($"bxc({program[pointer]}, {program[pointer + 1]})");
                     bxc(program[pointer], program[pointer + 1]);
                     break;
                 case 5:
+                    _logger.OnNext($"outInstruction({program[pointer]}, {program[pointer + 1]})");
                     outInstruction(program[pointer], program[pointer + 1]);
+                    if (_max == int.MaxValue)
+                        break;
+
+                    for (int i = 0; i < output.Count; i++)
+                    {
+                        if (output[i] != program[i])
+                        {
+                            return "";
+                        }
+                    }
                     break;
                 case 6:
+                    _logger.OnNext($"bdv({program[pointer]}, {program[pointer + 1]})");
                     bdv(program[pointer], program[pointer + 1]);
                     break;
                 case 7:
+                    _logger.OnNext($"cdv({program[pointer]}, {program[pointer + 1]})");
                     cdv(program[pointer], program[pointer + 1]);
                     break;
                 default:
@@ -83,11 +223,6 @@ public class Device
             }
 
             pointer += 2;
-
-            if (step > 10000)
-            {
-                throw new Exception("might have infinte loop?");
-            }
         }
         return string.Join(",", output);
     }
@@ -95,70 +230,75 @@ public class Device
     //The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
 
 
-    private void adv(int instruction, int operand)
+    private void adv(decimal instruction, decimal operand)
     {
         var combo = GetOperand(operand);
 
-        A = double.Floor(A / Math.Pow(combo, 2));
+        A = double.Floor(A / Math.Pow(2, combo));
     }
 
 
     //The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
-    private void bxl(int instruction, int operand)
+    private void bxl(decimal instruction, decimal operand)
     {
-        B = (int)B ^ operand;
+        //B = (int)B ^ (int)operand;
+        B = (double)Day17Solver.XorDecimals((decimal)B, operand);
     }
 
     // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
-    private void bst(int instruction, int operand)
+    private void bst(decimal instruction, decimal operand)
     {
         var combo = GetOperand(operand);
         B = combo % 8;
     }
 
     //The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
-    private void jnz(int instruction, int operand)
+    private void jnz(decimal instruction, decimal operand)
     {
         if (A != 0)
         {
-            pointer = operand;
+            pointer = (int)operand;
         }
     }
 
     //The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
-    private void bxc(int instruction, int operand)
+    private void bxc(decimal instruction, decimal operand)
     {
-        B = (int)B ^ (int)C;
+        B = (double)Day17Solver.XorDecimals((decimal)B, (decimal)C);
     }
 
     //The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
-    private void outInstruction(int instruction, int operand)
+    private void outInstruction(decimal instruction, decimal operand)
     {
         var combo = GetOperand(operand);
         output.Add(combo % 8);
     }
 
     //The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
-    private void bdv(int instruction, int operand)
+    private void bdv(decimal instruction, decimal operand)
     {
         var combo = GetOperand(operand);
-        B = double.Floor(A / Math.Pow(combo, 2));
+        B = double.Floor(A / Math.Pow(2, combo));
     }
 
     //The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
-    private void cdv(int instruction, int operand)
+    private void cdv(decimal instruction, decimal operand)
     {
         var combo = GetOperand(operand);
-        C = double.Floor(A / Math.Pow(combo, 2));
+        C = double.Floor(A / Math.Pow(2, combo));
     }
 
-    private double GetOperand(int o) => o switch
+    private double GetOperand(decimal o)
     {
-        < 4 => o,
-        4 => A,
-        5 => B,
-        6 => C,
-        _ => throw new ArgumentOutOfRangeException("7 is not valid for combo")
-    };
-
+        double v = o switch
+        {
+            < 4 => (int)o,
+            4 => A,
+            5 => B,
+            6 => C,
+            _ => throw new ArgumentOutOfRangeException("7 is not valid for combo")
+        };
+        _logger.OnNext($"GetOperand({o}) = {v}");
+        return v;
+    }
 }
